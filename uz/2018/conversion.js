7649c4b2449d859eeb0513eea97e5d3d8c8c979e
anlztrk <64364162+anlztrk@users.noreply.github.com>
2021-04-23 19:33:01 +0300

    updates

--- uz/2018/conversion.js
+++ uz/2018/conversion.js
@@ -50,6 +50,28 @@ translit = translit.replace(/SH/g, "Ş");
 translit = translit.replace(/sʼh/g, "sh");
 translit = translit.replace(/Sʼh/g, "Sh");
 translit = translit.replace(/SʼH/g, "SH");
+
+translit = translit.replace(/lŏbat/g, "lŏʼbat");
+translit = translit.replace(/mŏjaz/g, "mŏʼjaz");
+translit = translit.replace(/mŏjiza/g, "mŏʼjiza");
+translit = translit.replace(/mŏtabar/g, "mŏʼtabar");
+translit = translit.replace(/mŏtad/g, "mŏʼtad");
+translit = translit.replace(/mŏtazila/g, "mŏʼtazila");
+translit = translit.replace(/mŏtariza/g, "mŏʼtariza");
+translit = translit.replace(/Lŏbat/g, "Lŏʼbat");
+translit = translit.replace(/Mŏjaz/g, "Mŏʼjaz");
+translit = translit.replace(/Mŏjiza/g, "Mŏʼjiza");
+translit = translit.replace(/Mŏtabar/g, "Mŏʼtabar");
+translit = translit.replace(/Mŏtad/g, "Mŏʼtad");
+translit = translit.replace(/Mŏtazila/g, "Mŏʼtazila");
+translit = translit.replace(/Mŏtariza/g, "Mŏʼtariza");
+translit = translit.replace(/LŎBAT/g, "LŎʼBAT");
+translit = translit.replace(/MŎJAZ/g, "MŎʼJAZ");
+translit = translit.replace(/MŎJIZA/g, "MŎʼJIZA");
+translit = translit.replace(/MŎTABAR/g, "MŎʼTABAR");
+translit = translit.replace(/MŎTAD/g, "MŎʼTAD");
+translit = translit.replace(/MŎTAZILA/g, "MŎʼTAZILA");
+translit = translit.replace(/MŎTARIZA/g, "MŎʼTARIZA");
 document.transcription.text2.value = translit;
 }
 
@@ -94,6 +116,9 @@ translit = translit.replace(/ŏ/g, "oʻ");
 translit = translit.replace(/Ŏ/g, "Oʻ");
 translit = translit.replace(/ğ/g, "gʻ");
 translit = translit.replace(/Ğ/g, "Gʻ");
+
+translit = translit.replace(/oʻʼ/g, "oʻ");
+translit = translit.replace(/Oʻʼ/g, "Oʻ");
 document.transcription.text1.value = translit;
 }
 
--- uz/2021/conversion.js
+++ uz/2021/conversion.js
@@ -50,6 +50,28 @@ translit = translit.replace(/SH/g, "Ş");
 translit = translit.replace(/sʼh/g, "sh");
 translit = translit.replace(/Sʼh/g, "Sh");
 translit = translit.replace(/SʼH/g, "SH");
+
+translit = translit.replace(/lōbat/g, "lōʼbat");
+translit = translit.replace(/mōjaz/g, "mōʼjaz");
+translit = translit.replace(/mōjiza/g, "mōʼjiza");
+translit = translit.replace(/mōtabar/g, "mōʼtabar");
+translit = translit.replace(/mōtad/g, "mōʼtad");
+translit = translit.replace(/mōtazila/g, "mōʼtazila");
+translit = translit.replace(/mōtariza/g, "mōʼtariza");
+translit = translit.replace(/Lōbat/g, "Lōʼbat");
+translit = translit.replace(/Mōjaz/g, "Mōʼjaz");
+translit = translit.replace(/Mōjiza/g, "Mōʼjiza");
+translit = translit.replace(/Mōtabar/g, "Mōʼtabar");
+translit = translit.replace(/Mōtad/g, "Mōʼtad");
+translit = translit.replace(/Mōtazila/g, "Mōʼtazila");
+translit = translit.replace(/Mōtariza/g, "Mōʼtariza");
+translit = translit.replace(/LŌBAT/g, "LŌʼBAT");
+translit = translit.replace(/MŌJAZ/g, "MŌʼJAZ");
+translit = translit.replace(/MŌJIZA/g, "MŌʼJIZA");
+translit = translit.replace(/MŌTABAR/g, "MŌʼTABAR");
+translit = translit.replace(/MŌTAD/g, "MŌʼTAD");
+translit = translit.replace(/MŌTAZILA/g, "MŌʼTAZILA");
+translit = translit.replace(/MŌTARIZA/g, "MŌʼTARIZA");
 document.transcription.text2.value = translit;
 }
 
@@ -94,6 +116,9 @@ translit = translit.replace(/ō/g, "oʻ");
 translit = translit.replace(/Ō/g, "Oʻ");
 translit = translit.replace(/ḡ/g, "gʻ");
 translit = translit.replace(/Ḡ/g, "Gʻ");
+
+translit = translit.replace(/oʻʼ/g, "oʻ");
+translit = translit.replace(/Oʻʼ/g, "Oʻ");
 document.transcription.text1.value = translit;
 }
 
