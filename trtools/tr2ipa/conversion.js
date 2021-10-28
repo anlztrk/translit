27d6998057afd0b1d14c645d8c520353c1dcc46a
anlztrk <64364162+anlztrk@users.noreply.github.com>
2021-10-28 12:20:11 +0300

    Update conversion.js

--- trtools/tr2ipa/conversion.js
+++ trtools/tr2ipa/conversion.js
@@ -45,8 +45,10 @@ car = document.transcription.text1.value;
   car = car.replace(/V/g, "v");
   car = car.replace(/Z/g, "z");
 
-  car = car.replace(/(b|dʒ|tʃ|d|f|ɡ|h|ʒ|k|ɫ|m|n|p|ɾ|s|ʃ|t|v|j|z)(b|dʒ|tʃ|d|f|ɡ|h|ʒ|k|ɫ|m|n|p|ɾ|s|ʃ|t|v|j|z)(a|o|u|ɯ)/g, "$1ɯ$2$3");
-  car = car.replace(/(b|dʒ|tʃ|d|f|ɡ|h|ʒ|k|ɫ|m|n|p|ɾ|s|ʃ|t|v|j|z)(b|dʒ|tʃ|d|f|ɡ|h|ʒ|k|ɫ|m|n|p|ɾ|s|ʃ|t|v|j|z)(e|œ|y|i)/g, "$1i$2$3");
+  car = car.replace(/(b|dʒ|tʃ|d|f|ɡ|h|ʒ|k|ɫ|m|n|p|ɾ|s|ʃ|t|v|j|z)(b|dʒ|tʃ|d|f|ɡ|h|ʒ|k|ɫ|m|n|p|ɾ|s|ʃ|t|v|j|z)(a|ɯ)/g, "$1ɯ\.$2$3");
+  car = car.replace(/(b|dʒ|tʃ|d|f|ɡ|h|ʒ|k|ɫ|m|n|p|ɾ|s|ʃ|t|v|j|z)(b|dʒ|tʃ|d|f|ɡ|h|ʒ|k|ɫ|m|n|p|ɾ|s|ʃ|t|v|j|z)(o|u)/g, "$1u\.$2$3");
+  car = car.replace(/(b|dʒ|tʃ|d|f|ɡ|h|ʒ|k|ɫ|m|n|p|ɾ|s|ʃ|t|v|j|z)(b|dʒ|tʃ|d|f|ɡ|h|ʒ|k|ɫ|m|n|p|ɾ|s|ʃ|t|v|j|z)(e|i)/g, "$1i\.$2$3");
+  car = car.replace(/(b|dʒ|tʃ|d|f|ɡ|h|ʒ|k|ɫ|m|n|p|ɾ|s|ʃ|t|v|j|z)(b|dʒ|tʃ|d|f|ɡ|h|ʒ|k|ɫ|m|n|p|ɾ|s|ʃ|t|v|j|z)(œ|y)/g, "$1y\.$2$3");
   
   car = car.replace(/Â|â/g, "ʲa");
   car = car.replace(/Û|û/g, "ʲu");
