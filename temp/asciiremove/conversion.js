0aa9754a7114e238b2d49bd024b4ab1f4f15fa45
anlztrk <64364162+anlztrk@users.noreply.github.com>
2022-12-04 19:56:38 +0300

    update typeface

---
+++ temp/asciiremove/conversion.js
@@ -0,0 +1,11 @@
+var car;
+function cyrlat () {
+	car = document.transcription.text1.value;car = car.replace(/( |A|a|B|b|C|c|D|d|E|e|F|f|G|g|H|h|I|i|J|j|K|k|L|l|M|m|N|n|O|o|P|p|Q|q|R|r|S|s|T|t|U|u|V|v|W|w|X|x|Y|y|Z|z|\n|\r|\t)/g, "");
+document.transcription.text1.value=car;
+}
+function copy1()
+{ textRange=document.transcription.text1.createTextRange();   textRange.execCommand("Copy");   textRange="";
+}
+function copy2()
+{ textRange=document.transcription.text2.createTextRange();   textRange.execCommand("Copy");   textRange="";
+}
