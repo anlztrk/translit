fa4f625cdb3605d7a6bda4b73dd4cf617f29abd3
anlztrk <64364162+anlztrk@users.noreply.github.com>
2022-12-07 15:46:03 +0300

    Update conversion.js

--- trtools/engIPA/conversion.js
+++ trtools/engIPA/conversion.js
@@ -121,6 +121,11 @@ car = document.transcription.text1.value;
   car = car.replace(/e.(b|dʒ|tʃ|d|f|ɡ|ɟ|h|ʒ|k|c|ɫ|l|m|n|p|ɾ|s|ʃ|t|v|β|j|z)/g, "ɛ$1.");
   car = car.replace(/i.(b|dʒ|tʃ|d|f|ɡ|ɟ|h|ʒ|k|c|ɫ|l|m|n|p|ɾ|s|ʃ|t|v|β|j|z)/g, "ɪ$1.");
   car = car.replace(/(u|y).(b|dʒ|tʃ|d|f|ɡ|ɟ|h|ʒ|k|c|ɫ|l|m|n|p|ɾ|s|ʃ|t|v|β|j|z)/g, "ʊ$2.");
+  
+  car = car.replace(/a(b|dʒ|tʃ|d|f|ɡ|ɟ|h|ʒ|k|c|ɫ|l|m|n|p|ɾ|s|ʃ|t|v|β|j|z)/g, "ʌ$1.");
+  car = car.replace(/e(b|dʒ|tʃ|d|f|ɡ|ɟ|h|ʒ|k|c|ɫ|l|m|n|p|ɾ|s|ʃ|t|v|β|j|z)/g, "ɛ$1.");
+  car = car.replace(/i(b|dʒ|tʃ|d|f|ɡ|ɟ|h|ʒ|k|c|ɫ|l|m|n|p|ɾ|s|ʃ|t|v|β|j|z)/g, "ɪ$1.");
+  car = car.replace(/(u|y)(b|dʒ|tʃ|d|f|ɡ|ɟ|h|ʒ|k|c|ɫ|l|m|n|p|ɾ|s|ʃ|t|v|β|j|z)/g, "ʊ$2.");
 
   car = car.replace(/a/g, "ɑː");
   car = car.replace(/e/g, "eɪ");
