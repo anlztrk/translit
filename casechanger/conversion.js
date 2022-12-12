c58d134045561af2cd6f2e903e4816c4d62ba7ab
anlztrk <64364162+anlztrk@users.noreply.github.com>
2022-12-12 11:18:47 +0300

    Create conversion.js

---
+++ casechanger/conversion.js
@@ -0,0 +1,24 @@
+//  copyright lexilogos.com
+var car;
+function latcyr () {
+car = document.transcription.text2.value;
+translit = document.transcription.text2.value;
+  translit = translit.toUpperCase();
+  car = car.toLowerCase();
+   document.transcription.text2.value=translit;
+   document.transcription.text1.value=car;
+}
+function cyrlat () {
+car = document.transcription.text1.value;
+translit = document.transcription.text1.value;
+  translit = translit.toLowerCase();
+  car = car.toUpperCase();
+document.transcription.text1.value=translit;
+document.transcription.text2.value=car;
+}
+function copy1()
+{ textRange=document.transcription.text1.createTextRange();   textRange.execCommand("Copy");   textRange="";
+}
+function copy2()
+{ textRange=document.transcription.text2.createTextRange();   textRange.execCommand("Copy");   textRange="";
+}
