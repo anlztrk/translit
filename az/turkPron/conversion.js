638d087842ed4ab58a8566d65220c1ea3065901d
anlztrk <64364162+anlztrk@users.noreply.github.com>
2023-08-11 15:07:01 +0300

    Update conversion.js

--- az/turkPron/conversion.js
+++ az/turkPron/conversion.js
@@ -11,8 +11,8 @@ car = document.transcription.text1.value;
   
   car = car.replace(/(A|E|Ə|I|İ|O|Ö|U|Ü)Q(B|C|D|G|Ğ|J|L|M|N|R|V|Y|Z|b|c|d|g|ğ|j|l|m|n|r|v|y|z)/g, "$1Ğ$2");
   car = car.replace(/(A|E|Ə|I|İ|O|Ö|U|Ü|a|e|ə|ı|i|o|ö|u|ü)q(b|c|d|g|ğ|j|l|m|n|r|v|y|z)/g, "$1ğ$2");
-  car = car.replace(/(A|E|Ə|I|İ|O|Ö|U|Ü)Q/g, "$1X");
-  car = car.replace(/(A|E|Ə|I|İ|O|Ö|U|Ü|a|e|ə|ı|i|o|ö|u|ü)q/g, "$1x");
+  car = car.replace(/(A|E|Ə|I|İ|O|Ö|U|Ü)Q(^Q)/g, "$1X$3");
+  car = car.replace(/(A|E|Ə|I|İ|O|Ö|U|Ü|a|e|ə|ı|i|o|ö|u|ü)q(^q)/g, "$1x$3");
   car = car.replace(/X(A|E|Ə|I|İ|O|Ö|U|Ü|a|e|ə|ı|i|o|ö|u|ü)/g, "Q$1");
   car = car.replace(/x(a|e|ə|ı|i|o|ö|u|ü)/g, "q$1");
 
