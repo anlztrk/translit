06169e03ab49ce7e3965e29492dd4900f29830f9
anlztrk <64364162+anlztrk@users.noreply.github.com>
2023-08-11 14:37:03 +0300

    Update and rename az/alt/1/conversion.js to az/turkPron/conversion.js

---
+++ az/turkPron/conversion.js
@@ -0,0 +1,46 @@
+var car;
+function cyrlat () {
+car = document.transcription.text1.value;
+  car = car.replace(/x/g, "ḥ");
+  car = car.replace(/X/g, "Ḥ");
+  
+  car = car.replace(/(G|g)(a|o|u)/g, "$1$2\u0302");
+  car = car.replace(/G(A|O|U)/g, "G$1\u0302");
+  
+  car = car.replace(/(A|E|Ə|I|İ|O|Ö|U|Ü)Q(B|C|D|G|Ğ|J|Q|L|M|N|R|V|Y|Z|b|c|d|g|ğ|j|q|l|m|n|r|v|y|z)/g, "$1Ğ$2");
+  car = car.replace(/(A|E|Ə|I|İ|O|Ö|U|Ü|a|e|ə|ı|i|o|ö|u|ü)q(b|c|d|g|ğ|j|q|l|m|n|r|v|y|z)/g, "$1ğ$2");
+  car = car.replace(/(A|E|Ə|I|İ|O|Ö|U|Ü)Q/g, "$1X");
+  car = car.replace(/(A|E|Ə|I|İ|O|Ö|U|Ü|a|e|ə|ı|i|o|ö|u|ü)q/g, "$1x");
+  car = car.replace(/X(A|E|Ə|I|İ|O|Ö|U|Ü|a|e|ə|ı|i|o|ö|u|ü)/g, "Q$1");
+  car = car.replace(/x(a|e|ə|ı|i|o|ö|u|ü)/g, "q$1");
+
+  car = car.replace(/(E|Ə|İ|Ö|Ü)Q/g, "$1Ġ");
+  car = car.replace(/(E|Ə|İ|Ö|Ü|e|ə|i|ö|ü)q/g, "$1ġ");
+  car = car.replace(/Q(E|Ə|İ|Ö|Ü|e|ə|i|ö|ü)/g, "Ġ$1");
+  car = car.replace(/q(e|ə|i|ö|ü)/g, "ġ$1");
+  
+  car = car.replace(/(ġ)(a|o|u)/g, "g$2");
+  car = car.replace(/(Ġ)(A|O|U|a|o|u)/g, "G$2");
+
+  car = car.replace(/(E|Ə|İ|Ö|Ü)(B|C|Ç|D|F|G|Ğ|H|X|J|K|Q|L|M|N|P|R|S|Ş|T|V|Y|Z)Q/g, "$1$2Ġ");
+  
+  car = car.replace(/(E|Ə|İ|Ö|Ü|e|ə|i|ö|ü)(b|c|ç|d|f|g|ğ|h|x|j|k|q|l|m|n|p|r|s|ş|t|v|y|z)q/g, "$1$2ġ");
+  
+  car = car.replace(/q/g, "g");
+  car = car.replace(/Q/g, "G");
+  car = car.replace(/e/g, "é");
+  car = car.replace(/E/g, "É");
+  car = car.replace(/ə/g, "e");
+  car = car.replace(/Ə/g, "E");
+  car = car.replace(/x/g, "ḥ");
+  car = car.replace(/X/g, "Ḥ");
+document.transcription.text2.value=car;
+}
+function latcyr () {
+}
+function copy1()
+{ textRange=document.transcription.text1.createTextRange();   textRange.execCommand("Copy");   textRange="";
+}
+function copy2()
+{ textRange=document.transcription.text2.createTextRange();   textRange.execCommand("Copy");   textRange="";
+}
