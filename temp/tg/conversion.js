56237e857043201be35cfcb624945c1fa9ec85a8
anlztrk <64364162+anlztrk@users.noreply.github.com>
2023-11-13 12:46:36 +0300

    added /temp/tg

---
+++ temp/tg/conversion.js
@@ -0,0 +1,18 @@
+﻿//  copyright lexilogos.com
+var car;
+function latcyr () {
+}
+function cyrlat () {
+car = document.transcription.text1.value;
+  car = car.replace(/í/g, "ī");
+  car = car.replace(/Í/g, "Ī");
+  car = car.replace(/ú/g, "ū");
+  car = car.replace(/Ú/g, "Ū");
+document.transcription.text2.value=car;
+}
+function copy1()
+{ textRange=document.transcription.text1.createTextRange();   textRange.execCommand("Copy");   textRange="";
+}
+function copy2()
+{ textRange=document.transcription.text2.createTextRange();   textRange.execCommand("Copy");   textRange="";
+}
