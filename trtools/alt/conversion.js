e4236a9fac2a56bb5cbc105fdb465c8664acfea6
anlztrk <64364162+anlztrk@users.noreply.github.com>
2024-02-01 11:01:42 +0300

    Update conversion.js

--- trtools/alt/conversion.js
+++ trtools/alt/conversion.js
@@ -1,6 +1,6 @@
 var car;
 function cyrlat () {
-car = document.transcription.text2.value;
+car = document.transcription.text1.value;
   car = car.replace(/i/g, "ï");
   car = car.replace(/ı/g, "i");
   car = car.replace(/j/g, "z\u0327");
@@ -10,7 +10,7 @@ car = document.transcription.text2.value;
   car = car.replace(/J/g, "Z\u0327");
   car = car.replace(/C/g, "J");
   car = car.replace(/Ç/g, "C");
-   document.transcription.text1.value=car;
+   document.transcription.text2.value=car;
 }
 function copy1()
 { textRange=document.transcription.text1.createTextRange();   textRange.execCommand("Copy");   textRange="";
