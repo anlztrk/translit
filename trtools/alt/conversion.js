e493266be26afdb58b4e28d0fab4dadd261b64b8
anlztrk <64364162+anlztrk@users.noreply.github.com>
2024-02-01 10:57:22 +0300

    Create conversion.js

---
+++ trtools/alt/conversion.js
@@ -0,0 +1,20 @@
+var car;
+function latcyr () {
+car = document.transcription.text2.value;
+  car = car.replace(/i/g, "ï");
+  car = car.replace(/ı/g, "i");
+  car = car.replace(/j/g, "z\u0327");
+  car = car.replace(/c/g, "j");
+  car = car.replace(/ç/g, "c");
+  car = car.replace(/İ/g, "Ï");  
+  car = car.replace(/J/g, "Z\u0327");
+  car = car.replace(/C/g, "J");
+  car = car.replace(/Ç/g, "C");
+   document.transcription.text1.value=car;
+}
+function copy1()
+{ textRange=document.transcription.text1.createTextRange();   textRange.execCommand("Copy");   textRange="";
+}
+function copy2()
+{ textRange=document.transcription.text2.createTextRange();   textRange.execCommand("Copy");   textRange="";
+}
