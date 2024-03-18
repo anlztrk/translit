1850ff4cc877c390b1cf2ae4f78795fb43d0abb7
anlztrk <64364162+anlztrk@users.noreply.github.com>
2024-03-18 14:18:02 +0300

    Update conversion.js

--- input/diacritic/conversion.js
+++ input/diacritic/conversion.js
@@ -2,54 +2,39 @@ var car;
 var diacritic;
 
 function cyrlat() {
-	car = document.transcription.text1.value;
-	diacritic = toLowercase(document.transcription.text3.value);
+  car = document.transcription.text1.value;
+  diacritic = toLowercase(document.transcription.text3.value);
   if (diacritic == "acute"){
 	document.transcription.text2.value = concat(car,"\u0301");
-  }
-  else if (diacritic == "grave"){
+  }  else if (diacritic == "grave"){
   document.transcription.text2.value = concat(car,"\u0300");
-  }
-  else if (diacritic == "circumflex"){
+  }  else if (diacritic == "circumflex"){
   document.transcription.text2.value = concat(car,"\u0302");
-  }
-  else if (diacritic == "tilde"){
+  }  else if (diacritic == "tilde"){
   document.transcription.text2.value = concat(car,"\u0303");
-  }
-  else if (diacritic == "macron"){
+  }  else if (diacritic == "macron"){
   document.transcription.text2.value = concat(car,"\u0304");
-  }
-  else if (diacritic == "hacek" || diacritic == "hachek" || diacritic == "caron" || diacritic == "haček" || diacritic == "háček"){
+  }  else if (diacritic == "hacek" || diacritic == "hachek" || diacritic == "caron" || diacritic == "haček" || diacritic == "háček"){
   document.transcription.text2.value = concat(car,"\u030c");
-  }
-  else if (diacritic == "breve"){
+  }  else if (diacritic == "breve"){
   document.transcription.text2.value = concat(car,"\u0306");
-  }
-  else if (diacritic == "umlaut" || diacritic == "diaeresis" || diacritic == "trema" || diacritic == "tréma"){
+  }  else if (diacritic == "umlaut" || diacritic == "diaeresis" || diacritic == "trema" || diacritic == "tréma"){
   document.transcription.text2.value = concat(car,"\u0308");
-  }
-  else if (diacritic == "dotabove"){
+  }  else if (diacritic == "dotabove"){
   document.transcription.text2.value = concat(car,"\u0307");
-  }
-  else if (diacritic == "hook"){
+  }  else if (diacritic == "hook"){
   document.transcription.text2.value = concat(car,"\u0309");
-  }
-  else if (diacritic == "ringabove" || diacritic == "ring"){
+  }  else if (diacritic == "ringabove" || diacritic == "ring"){
   document.transcription.text2.value = concat(car,"\u030a");
-  }
-  else if (diacritic == "dotbelow"){
+  }  else if (diacritic == "dotbelow"){
   document.transcription.text2.value = concat(car,"\u0323");
-  }
-  else if (diacritic == "doubleacute"){
+  }  else if (diacritic == "doubleacute"){
   document.transcription.text2.value = concat(car,"\u030b");
-  }
-  else if (diacritic == "cedilla"){
+  }  else if (diacritic == "cedilla"){
   document.transcription.text2.value = concat(car,"\u0327");
-  }
-  else if (diacritic == "ogonek"){
+  }  else if (diacritic == "ogonek"){
   document.transcription.text2.value = concat(car,"\u0328");
-  }
-  else {
+  }  else {
   document.transcription.text2.value = car;
   }
 }
