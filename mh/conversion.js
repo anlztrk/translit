df71deed26b63eaf8edc78ae58285d55f0c9dd9d
anlztrk <64364162+anlztrk@users.noreply.github.com>
2024-03-20 16:01:52 +0300

    Update conversion.js

--- mh/conversion.js
+++ mh/conversion.js
@@ -5,7 +5,7 @@ car = document.transcription.text1.value;
 car = car.normalize('NFD');
 car = car.replace(/\u200c/g, "");
 car = car.replace(/\u0327/g, "\u0323");
-car = car.replace(/n\u0304/g, "n\u0303");
+car = car.replace(/(N|n)\u0304/g, "$1\u0303");
 car = car.normalize('NFC');
 document.transcription.text2.value = car;
 }
@@ -13,7 +13,7 @@ function latcyr () {
 car = document.transcription.text2.value;
 car = car.normalize('NFD');
 car = car.replace(/\u0323/g, "\u0327");
-car = car.replace(/n\u0303/g, "n\u0304");
+car = car.replace(/(N|n)\u0303/g, "$1\u0304");
 car = car.normalize('NFC');
 document.transcription.text1.value = car;
 }
