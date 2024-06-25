28694848f12bcf4156bfc112d0ab7015dfae8525
anlztrk <64364162+anlztrk@users.noreply.github.com>
2024-06-25 14:49:33 +0300

    Update conversion.js

--- temp/tg/conversion.js
+++ temp/tg/conversion.js
@@ -4,10 +4,26 @@ function latcyr () {
 }
 function cyrlat () {
 car = document.transcription.text1.value;
-  car = car.replace(/í/g, "ī");
-  car = car.replace(/Í/g, "Ī");
-  car = car.replace(/ú/g, "ū");
-  car = car.replace(/Ú/g, "Ū");
+  car = car.replace(/e/g, "ê");
+  car = car.replace(/E/g, "Ê");
+  car = car.replace(/a/g, "e");
+  car = car.replace(/A/g, "E");
+  car = car.replace(/o/g, "a");
+  car = car.replace(/O/g, "A");
+  car = car.replace(/ī/g, "î");
+  car = car.replace(/Ī/g, "Î");
+  car = car.replace(/ū/g, "û");
+  car = car.replace(/Ū/g, "Û");
+  car = car.replace(/ǧ/g, "ẍ");
+  car = car.replace(/Ǧ/g, "Ẍ");
+  car = car.replace(/š/g, "ş");
+  car = car.replace(/Š/g, "Ş");
+  car = car.replace(/c/g, "ç");
+  car = car.replace(/C/g, "Ç");
+  car = car.replace(/j/g, "c");
+  car = car.replace(/J/g, "C");
+  car = car.replace(/ž/g, "j");
+  car = car.replace(/Ž/g, "J");
 document.transcription.text2.value=car;
 }
 function copy1()
