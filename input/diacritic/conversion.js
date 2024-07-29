0bc471895ebb1395f2267bee7ff0d3e42385a7c2
anlztrk <64364162+anlztrk@users.noreply.github.com>
2024-07-29 10:16:09 +0300

    Update conversion.js

--- input/diacritic/conversion.js
+++ input/diacritic/conversion.js
@@ -19,7 +19,7 @@ car = car.replace(/\u0023comma(below)?\u0023/g, "\u0326");
 car = car.replace(/\u0023(cedilla|tail)\u0023/g, "\u0327");
 car = car.replace(/\u0023ogonek\u0023/g, "\u0328");
 car = car.replace(/\u0023(vertical)?line(below)?\u0023/g, "\u0329");
-car = car.replace(/\u0023(circumflex|caret)?below\u0023/g, "\u032d");
+car = car.replace(/\u0023(circumflex|caret)below\u0023/g, "\u032d");
 car = car.normalize('NFC');
 document.transcription.text1.value=car;
 }
