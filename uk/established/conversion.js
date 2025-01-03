b1cd643d7364c1f2e1b9f4cc0faba58d96a37da4
anlztrk <64364162+anlztrk@users.noreply.github.com>
2025-01-03 14:25:28 +0300

    Update conversion.js

--- uk/established/conversion.js
+++ uk/established/conversion.js
@@ -115,8 +115,10 @@ car = document.transcription.text1.value;
 
   car = car.replace(/(B|C|D|F|G|H|I|K|L|M|N|P|R|S|T|V|Y|Z)ẎI/g, "$1I");
   car = car.replace(/(B|C|D|F|G|H|I|K|L|M|N|P|R|S|T|V|Y|Z|b|c|d|f|g|h|i|k|l|m|n|p|r|s|t|v|y|z)ẏi/g, "$1i");
-  car = car.replace(/(A|E|I|O|U|Y)Ẏ(?!A|E|I|O|U|Y|Ẏ)?/g, "$1I$2");
-  car = car.replace(/(A|E|I|O|U|Y|a|e|i|o|u)ẏ(?!a|e|i|o|u|y|ẏ)?/g, "$1i$2");
+  car = car.replace(/Ẏ(?!A|E|I|O|U|Y|Ẏ|a|e|i|o|u|y|ẏ)/g, "I$1");
+  car = car.replace(/ẏ(?!a|e|i|o|u|y|ẏ)/g, "i$1");
+  car = car.replace(/(B|C|D|F|G|H|K|L|M|N|P|R|S|T|V|Z)Ẏ/g, "$1I");
+  car = car.replace(/(B|C|D|F|G|H|K|L|M|N|P|R|S|T|V|Z|b|c|d|f|g|h|k|l|m|n|p|r|s|t|v|z)ẏ/g, "$1i");  
   car = car.replace(/Ẏ/g, "Y");
   car = car.replace(/ẏ/g, "y");
   car = car.normalize('NFC');
