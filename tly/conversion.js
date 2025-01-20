06a61ee06b825f1955abd740e9bfb47eeaa5897e
anlztrk <64364162+anlztrk@users.noreply.github.com>
2025-01-20 15:16:20 +0300

    Create conversion.js

---
+++ tly/conversion.js
@@ -0,0 +1,49 @@
+
+var car;
+var translit;
+function cyrlat () {
+car = document.transcription.text2.value;
+  car = car.replace(/č/g, "c");
+  car = car.replace(/c/g, "ç");
+  car = car.replace(/ǧ/g, "ğ");
+  car = car.replace(/y/g, "ı");
+  car = car.replace(/j/g, "y");
+  car = car.replace(/ž/g, "j");
+  car = car.replace(/g/g, "q");
+
+  car = car.replace(/Č/g, "C");
+  car = car.replace(/C/g, "Ç");
+  car = car.replace(/Ǧ/g, "Ğ");
+  car = car.replace(/I/g, "İ");
+  car = car.replace(/Y/g, "I");
+  car = car.replace(/J/g, "Y");
+  car = car.replace(/Ž/g, "J");
+  car = car.replace(/G/g, "Q");
+document.transcription.text1.value=car;
+}
+function latcyr () {
+car = document.transcription.text1.value;
+  car = car.replace(/ç/g, "c");
+  car = car.replace(/c/g, "č");
+  car = car.replace(/ğ/g, "ǧ");
+  car = car.replace(/j/g, "ž");
+  car = car.replace(/y/g, "j");
+  car = car.replace(/ı/g, "y");
+  car = car.replace(/q/g, "g");
+
+  car = car.replace(/Ç/g, "C");
+  car = car.replace(/C/g, "Č");
+  car = car.replace(/Ğ/g, "Ǧ");
+  car = car.replace(/J/g, "Ž");
+  car = car.replace(/Y/g, "J");
+  car = car.replace(/I/g, "Y");
+  car = car.replace(/İ/g, "I");
+  car = car.replace(/Q/g, "G");
+document.transcription.text2.value=car;
+}
+function copy1()
+{ textRange=document.transcription.text1.createTextRange();   textRange.execCommand("Copy");   textRange="";
+}
+function copy2()
+{ textRange=document.transcription.text2.createTextRange();   textRange.execCommand("Copy");   textRange="";
+}
