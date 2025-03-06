a724bcd2c654a89e4b26eefa856bdbf023ab6603
anlztrk <64364162+anlztrk@users.noreply.github.com>
2025-03-06 11:20:27 +0300

    Update conversion.js

--- ug/arb2lat/conversion.js
+++ ug/arb2lat/conversion.js
@@ -63,8 +63,8 @@ function latcyr () {
   car = car.replace(/\u02bc/g, "\u2019"); 
   car = car.replace(/\u0027/g, "\u2019");
 
-  car = car.replace(/(B|D|F|G|H|J|K|L|M|N|P|Q|R|S|T|W|X|Y|Z)(A|E|Ë|I|O|Ö|U|Ü)/g, "$1\u200c$2");
-  car = car.replace(/(B|D|F|G|H|J|K|L|M|N|P|Q|R|S|T|W|X|Y|Z|b|d|f|g|h|j|k|l|m|n|p|q|r|s|t|w|x|y|z)(a|e|ë|i|o|ö|u|ü)/g, "$1\u200c$2");
+  car = car.replace(/(B|D|F|G|H|J|K|L|M|N|P|Q|R|S|T|W|X|Y|Z|\u2019)(A|E|Ë|I|O|Ö|U|Ü)/g, "$1\u200c$2");
+  car = car.replace(/(B|D|F|G|H|J|K|L|M|N|P|Q|R|S|T|W|X|Y|Z|b|d|f|g|h|j|k|l|m|n|p|q|r|s|t|w|x|y|z|\u2019)(a|e|ë|i|o|ö|u|ü)/g, "$1\u200c$2");
 
   car = car.replace(/(CH|Ch|ch)/g, "چ"); 
   car = car.replace(/(GH|Gh|gh)/g, "غ"); 
