e8d8b5a19b35b194d20b56c5aac817de1cf5304c
anlztrk <64364162+anlztrk@users.noreply.github.com>
2025-04-02 15:17:21 +0300

    Update conversion.js

--- input/diacritic/conversion.js
+++ input/diacritic/conversion.js
@@ -22,7 +22,7 @@ car = car.replace(/\u0023(cedilla|tail)\u0023/g, "\u0327");
 car = car.replace(/\u0023ogonek\u0023/g, "\u0328");
 car = car.replace(/\u0023(vertical)?line(below)?\u0023/g, "\u0329");
 car = car.replace(/\u0023(circumflex|caret)below\u0023/g, "\u032d");
-car = car.replace(/\u0023U\+([0-9A-Fa-f]+)|u([0-9A-Fa-f]{4})\u0023/g, (_, uPlus, uLower) => {
+car = car.replace(/U\+([0-9A-Fa-f]+)|u([0-9A-Fa-f]{4})/g, (_, uPlus, uLower) => {
     const hex = uPlus || uLower;
     return String.fromCodePoint(parseInt(hex, 16));
   });
