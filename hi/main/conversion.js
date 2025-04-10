4c31ca70ef726d480a2ff07881916a92c6bbc874
anlztrk <64364162+anlztrk@users.noreply.github.com>
2025-04-10 12:39:20 +0300

    Update conversion.js

--- hi/main/conversion.js
+++ hi/main/conversion.js
@@ -13,10 +13,13 @@ function cyrlat() {
 		return key;
 	});
 
+	// Nasal + consonant rules
 	car = car.replace(/(ं|ँ)(क|ख|ग|घ|ह)/g, "N$2");
 	car = car.replace(/(ं|ँ)(त|थ|द|ध|ल|स)/g, "N$2");
 	car = car.replace(/(ं|ँ)(प|फ|ब|भ|व)/g, "M$2");
 	car = car.replace(/(ं|ँ)(?![\u093e-\u094c])/g, "N");
+
+	// Special consonants with nukta
 	car = car.replace(/क़/g, "\u200bQ\u200c");
 	car = car.replace(/ख़/g, "\u200bKH\u200c");
 	car = car.replace(/ग़/g, "\u200bGH\u200c");
@@ -26,6 +29,8 @@ function cyrlat() {
 	car = car.replace(/ड़/g, "\u200bR\u200c");
 	car = car.replace(/ढ़/g, "\u200bRH\u200c");
 	car = car.replace(/ज्ञ/g, "\u200bGY\u200c");
+
+	// Regular consonants
 	car = car.replace(/क/g, "\u200bK\u200c");
 	car = car.replace(/ख/g, "\u200bKH\u200c");
 	car = car.replace(/ग/g, "\u200bG\u200c");
@@ -61,6 +66,8 @@ function cyrlat() {
 	car = car.replace(/व/g, "\u200bW\u200c");
 	car = car.replace(/ळ/g, "\u200bL\u200c");
 	car = car.replace(/व़/g, "\u200bW\u200c");
+
+	// Independent vowels
 	car = car.replace(/अ/g, "\u200bA");
 	car = car.replace(/इ/g, "\u200bI");
 	car = car.replace(/उ/g, "\u200bU");
@@ -72,9 +79,8 @@ function cyrlat() {
 	car = car.replace(/ऊ/g, "\u200bOO");
 	car = car.replace(/ऐ/g, "\u200bAI");
 	car = car.replace(/औ/g, "\u200bAU");
-	car = car.replace(/(\u200b[^\u200b\u200c\s]{1,2})\u200c([िुृेोाीूैौ])/g, "$1$2");
-	car = car.replace(/\u200c्/g, "");
 
+	// Matras (dependent vowels)
 	car = car.replace(/ि/g, "I");
 	car = car.replace(/ु/g, "U");
 	car = car.replace(/ृ/g, "RI");
@@ -85,16 +91,19 @@ function cyrlat() {
 	car = car.replace(/ू/g, "OO");
 	car = car.replace(/ै/g, "AI");
 	car = car.replace(/ौ/g, "AU");
+
+	// Visarga
 	car = car.replace(/ः/g, "\u200bH");
-	// Protect first schwa in each word, delete others
-	car = car.replace(/\S+/g, function(word) {
+
+	// Protect first schwa in each sequence of consonant-schwa patterns
+	car = car.replace(/((?:\u200b[^\u200b\u200c\s]{1,2}\u200c)+)/g, function(group) {
 		let protected = false;
-		return word.replace(/(\u200b[^\u200b\u200c\s]{1,2})\u200c/g, function(match, cons) {
+		return group.replace(/(\u200b[^\u200b\u200c\s]{1,2})\u200c/g, function(_, cons) {
 			if (!protected) {
 				protected = true;
-				return cons + "#"; // protect first schwa
+				return cons + "#";
 			} else {
-				return cons + "\u200c\u200b"; // delete others
+				return cons + "\u200c\u200b";
 			}
 		});
 	});
@@ -108,31 +117,25 @@ function cyrlat() {
 	// Reinsert schwa in 3-consonant clusters → CəCC
 	car = car.replace(/(\u200b[^\u200b\u200c\s]{1,2})(?!\u200c)(\u200b[^\u200b\u200c\s]{1,2})(?!\u200c)(\u200b[^\u200b\u200c\s]{1,2})/g, "$1\u200c$2$3");
 
-	car = car.replace(/\u200c(?!\u200b)/g, "A"); // keep realized schwas
-	car = car.replace(/\u200c\u200b/g, "");     // delete marked schwas
-	car = car.replace(/\u200b/g, "");           // cleanup
-	car = car.replace(/\u200c/g, "");
+	// Realize and delete schwas
+	car = car.replace(/\u200c(?!\u200b)/g, "A"); // Realized schwa
+	car = car.replace(/\u200c\u200b/g, "");     // Deleted schwa
+	car = car.replace(/\u200b/g, "");           // Cleanup ZWSP
+
+	// Other punctuation handling
 	car = car.replace(/\u0964/g, "\u002e");
 	car = car.replace(/\u0965/g, "");
+
+	// Normalize and fix casing
 	car = car.normalize('NFC');
 	car = car.replace(/(\p{L}|\p{N}|__placeholder\d+__)([\p{L}\t\u0020,;\u002d\u2010\u201c\u201d\u2018\u2019'"()]+)/gu, function(_, first, second) {
 		return first + second.toLowerCase();
 	});
 
+	// Restore placeholders
 	Object.keys(latinWords).forEach(key => {
 		car = car.replace(key, latinWords[key]);
 	});
+
 	document.transcription.text2.value = car;
-}
-
-function copy1() {
-	textRange = document.transcription.text1.createTextRange();
-	textRange.execCommand("Copy");
-	textRange = "";
-}
-
-function copy2() {
-	textRange = document.transcription.text2.createTextRange();
-	textRange.execCommand("Copy");
-	textRange = "";
 }
