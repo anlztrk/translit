081f0cb1854d75b68bc79e568bb2bb46056d4fab
anlztrk <64364162+anlztrk@users.noreply.github.com>
2025-04-17 16:40:09 +0300

    Update conversion.js

--- tt/translit/zamanalif/conversion.js
+++ tt/translit/zamanalif/conversion.js
@@ -1,6 +1,8 @@
 var car;
 function cyrlat () {
 car = document.transcription.text1.value;
+car = car.replace(/([БВГДЖҖЗЙКЛМНҢПРСТФХҺЦЧШЩ])Е/g, "$1Э");
+car = car.replace(/([БВГДЖҖЗЙКЛМНҢПРСТФХҺЦЧШЩбвгджҗзйклмнңпрстфхһцчшщ])е/g, "$1э");
 
 const rules = [
   {
@@ -34,19 +36,34 @@ const rules = [
     replace: "йү$1$2",
   },
   {
+    test:    /ҮЕ/,
+    pattern: /ҮЕ/g,
+    replace: "ҮЭ",
+  },
+  {
+    test:    /Үе/,
+    pattern: /Үе/g,
+    replace: "Үэ",
+  },
+  {
+    test:    /үе/,
+    pattern: /үе/g,
+    replace: "үэ",
+  },
+  {
     test:    /Е[БВГДЖҖЗЙКЛМНҢПРСТФХҺЦЧШЩ]{0,3}[ӘЭИӨҮЬ]/,
     pattern: /Е([БВГДЖҖЗЙКЛМНҢПРСТФХҺЦЧШЩ]{0,3})([ӘЭИӨҮЬ])/g,
-    replace: "Э$1$2",
+    replace: "ЙЭ$1$2",
   },
   {
     test:    /Е[бвгджҗзйклмнңпрстфхһцчшщ]{0,3}[әэиөүь]/,
     pattern: /Е([бвгджҗзйклмнңпрстфхһцчшщ]{0,3})([әэиөүь])/g,
-    replace: "Э$1$2",
+    replace: "Йэ$1$2",
   },
   {
     test:    /е[бвгджҗзйклмнңпрстфхһцчшщ]{0,3}[әэиөүь]/,
     pattern: /е([бвгджҗзйклмнңпрстфхһцчшщ]{0,3})([әэиөүь])/g,
-    replace: "э$1$2",
+    replace: "йэ$1$2",
   }
 ];
 
@@ -61,7 +78,26 @@ while (changed) {
     }
   }
 }
-  
+  car = car.replace(/(\p{Uppercase})(\p{Uppercase}) Я/ug, "$1$2 ЙА");
+  car = car.replace(/Я (\p{Uppercase})(\p{Uppercase})/ug, "ЙА $1$2");
+  car = car.replace(/Я(\p{Uppercase})/ug, "ЙА$1");
+  car = car.replace(/Я/g, "Йа");
+  car = car.replace(/я/g, "йа");
+  car = car.replace(/(\p{Uppercase})(\p{Uppercase}) Е/ug, "$1$2 ЙЭ");
+  car = car.replace(/Е (\p{Uppercase})(\p{Uppercase})/ug, "ЙЫ $1$2");
+  car = car.replace(/Е(\p{Uppercase})/g, "ЙЫ$1");
+  car = car.replace(/Е/g, "Йы");
+  car = car.replace(/е/g, "йы");
+  car = car.replace(/(\p{Uppercase})(\p{Uppercase}) Ё/ug, "$1$2 ЙО");
+  car = car.replace(/Ё (\p{Uppercase})(\p{Uppercase})/ug, "ЙО $1$2");
+  car = car.replace(/Ё(\p{Uppercase})/ug, "ЙО$1");
+  car = car.replace(/Ё/g, "Йо");
+  car = car.replace(/ё/g, "йо");
+  car = car.replace(/(\p{Uppercase})(\p{Uppercase}) Ю/ug, "$1$2 ЙУ");
+  car = car.replace(/Ю (\p{Uppercase})(\p{Uppercase})/ug, "ЙУ $1$2");
+  car = car.replace(/Ю(\p{Uppercase})/ug, "ЙУ$1");
+  car = car.replace(/Ю/g, "Йу");
+  car = car.replace(/ю/g, "йу");
 document.transcription.text2.value=car;
 }
 function copy1()
