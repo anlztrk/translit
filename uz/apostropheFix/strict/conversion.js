e0d4b123a5e3045d1c702552ac42d6a2289341fa
anlztrk <64364162+anlztrk@users.noreply.github.com>
2025-05-06 13:56:47 +0300

    Update conversion.js

--- uz/apostropheFix/strict/conversion.js
+++ uz/apostropheFix/strict/conversion.js
@@ -1,10 +1,6 @@
-﻿
-var car;
 function cyrlat () {
-car = document.transcription.text1.value;
-  car = car.replace(/['ˈ’ʼ‘ʻ´`ʿʹˊ׳′ʽߵ՚ߴ᾿ՙ῾‛ʾ՛ˋ]/g, "ʼ");
-  car = car.replace(/([OoGg])ʼ/g, "$1ʻ");
-document.transcription.text1.value=car;
+document.transcription.text1.value = document.transcription.text1.value.replace(/['ˈ’ʼ‘ʻ´`ʿʹˊ׳′ʽߵ՚ߴ᾿ՙ῾‛ʾ՛ˋ]/g, "ʼ")
+                                                                       .replace(/([OoGg])ʼ/g, "$1ʻ");
 }
 function copy1()
 { textRange=document.transcription.text1.createTextRange();   textRange.execCommand("Copy");   textRange="";
