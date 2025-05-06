02a3805cec0d3092563635d51ef42080e981c37a
anlztrk <64364162+anlztrk@users.noreply.github.com>
2025-05-06 13:45:28 +0300

    Update conversion.js

--- uz/apostropheFix/official/conversion.js
+++ uz/apostropheFix/official/conversion.js
@@ -2,35 +2,8 @@
 var car;
 function cyrlat () {
 car = document.transcription.text1.value;
-  car = car.replace(/'/g, "’");
-  car = car.replace(/ˈ/g, "’");
-  car = car.replace(/’/g, "’");
-  car = car.replace(/ʼ/g, "’");
-  car = car.replace(/‘/g, "’");
-  car = car.replace(/ʻ/g, "’");
-  car = car.replace(/´/g, "’");
-  car = car.replace(/`/g, "’");
-  car = car.replace(/ʿ/g, "’");
-  car = car.replace(/ʹ/g, "’");
-  car = car.replace(/ˊ/g, "’");
-  car = car.replace(/׳/g, "’");
-  car = car.replace(/′/g, "’");
-  car = car.replace(/ʽ/g, "’");
-  car = car.replace(/ߵ/g, "’");
-  car = car.replace(/՚/g, "’");
-  car = car.replace(/ߴ/g, "’");
-  car = car.replace(/᾿/g, "’");
-  car = car.replace(/ՙ/g, "’");
-  car = car.replace(/῾/g, "’");
-  car = car.replace(/‛/g, "’");
-  car = car.replace(/ʾ/g, "’");
-  car = car.replace(/՛/g, "’");
-  car = car.replace(/ˋ/g, "’");
-
-  car = car.replace(/o’/g, "o‘");
-  car = car.replace(/O’/g, "O‘");
-  car = car.replace(/g’/g, "g‘");
-  car = car.replace(/G’/g, "G‘");
+  car = car.replace(/['ˈ’ʼ‘ʻ´`ʿʹˊ׳′ʽߵ՚ߴ᾿ՙ῾‛ʾ՛ˋ]/g, "’");
+  car = car.replace(/([OoGg])’/g, "$1‘");
 document.transcription.text1.value=car;
 }
 function copy1()
