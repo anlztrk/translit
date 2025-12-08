8397619e71e78811ffd7ae4c56e71856e5b23775
anlztrk <64364162+anlztrk@users.noreply.github.com>
2025-12-08 12:13:51 +0300

    Update conversion.js

--- trtools/tr2ipa/lt/conversion.js
+++ trtools/tr2ipa/lt/conversion.js
@@ -55,7 +55,8 @@ car = document.transcription.text1.value;
   car = car.replace(/ĵ/g, "j");
   car = car.replace(/(a|i|o|u|y|œ|ɯ)ɰ/g, "$1ː");
   car = car.replace(/a\.i/g, "aː.i");
-  car = car.replace(/ɰ/g, "");
+	car = car.replace(/a\.ɯ/g, "a.a");
+	car = car.replace(/ɰ/g, "");
 
   car = car.replace(/(o|u|y|œ)v/g, "$1β");
   car = car.replace(/v(o|u|y|œ)/g, "β$1");
