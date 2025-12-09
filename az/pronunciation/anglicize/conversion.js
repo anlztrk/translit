839f5a3c8adc3c6de558ce993e8b3a5fc9900545
anlztrk <64364162+anlztrk@users.noreply.github.com>
2025-12-09 13:53:42 +0300

    Update conversion.js

--- az/pronunciation/anglicize/conversion.js
+++ az/pronunciation/anglicize/conversion.js
@@ -1,41 +1,39 @@
-function cyrlat () {
-var car;
-var translit;
-car = document.transcription.text1.value;
-car = car.replace(/Ä/g, "Ə");
-car = car.replace(/ä/g, "ə");
-translit = car;
-car = car.replace(/Ə(\p{L})/ug, "A$1");
-car = car.replace(/ə(\p{L})/ug, "a$1");
-car = car.replace(/Ə/g, "EH");
-car = car.replace(/J/g, "Zh");
-car = car.replace(/C/g, "J");
-car = car.replace(/Ç/g, "Ch");
-car = car.replace(/Ğ/g, "GH");
-car = car.replace(/X/g, "Kh");
-car = car.replace(/İ/g, "I");
-car = car.replace(/Ş/g, "Sh");
-car = car.replace(/Q/g, "G");
-car = car.replace(/Ö/g, "O");
-car = car.replace(/Ü/g, "U");
-car = car.replace(/ə/g, "eh");
-car = car.replace(/j/g, "zh");
-car = car.replace(/c/g, "j");
-car = car.replace(/ç/g, "ch");
-car = car.replace(/ğ/g, "gh");
-car = car.replace(/x/g, "kh");
-car = car.replace(/ı/g, "i");
-car = car.replace(/ş/g, "sh");
-car = car.replace(/q/g, "g");
-car = car.replace(/ö/g, "o");
-car = car.replace(/ü/g, "u");
-document.transcription.text1.value=translit;
-document.transcription.text2.value=car;
-}
-function copy1()
-{ navigator.clipboard.writeText(document.transcription.text1.value);
+function cyrlat() {
+	document.transcription.text1.value = document.transcription.text1.value
+		.replace(/Ä/g, "Ə")
+		.replace(/ä/g, "ə");
+
+	document.transcription.text2.value = document.transcription.text1.value
+		.replace(/Ə(\p{L})/ug, "A$1")
+		.replace(/ə(\p{L})/ug, "a$1")
+		.replace(/Ə/g, "EH")
+		.replace(/J/g, "Zh")
+		.replace(/C/g, "J")
+		.replace(/Ç/g, "Ch")
+		.replace(/Ğ/g, "GH")
+		.replace(/X/g, "Kh")
+		.replace(/İ/g, "I")
+		.replace(/Ş/g, "Sh")
+		.replace(/Q/g, "G")
+		.replace(/Ö/g, "O")
+		.replace(/Ü/g, "U")
+		.replace(/ə/g, "eh")
+		.replace(/j/g, "zh")
+		.replace(/c/g, "j")
+		.replace(/ç/g, "ch")
+		.replace(/ğ/g, "gh")
+		.replace(/x/g, "kh")
+		.replace(/ı/g, "i")
+		.replace(/ş/g, "sh")
+		.replace(/q/g, "g")
+		.replace(/ö/g, "o")
+		.replace(/ü/g, "u");
 }
 
-function copy2()
-{ navigator.clipboard.writeText(document.transcription.text2.value);
+function copy1() {
+	navigator.clipboard.writeText(document.transcription.text1.value);
+}
+
+function copy2() {
+	navigator.clipboard.writeText(document.transcription.text2.value);
 }
