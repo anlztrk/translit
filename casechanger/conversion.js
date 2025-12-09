abb6c064b22d5accd03628b398021566b9957b1a
anlztrk <64364162+anlztrk@users.noreply.github.com>
2025-12-09 16:07:28 +0300

    Update conversion.js

--- casechanger/conversion.js
+++ casechanger/conversion.js
@@ -1,26 +1,25 @@
-var car;
-function latcyr () {
-car = document.transcription.text2.value;
-translit = document.transcription.text2.value;
-  translit = translit.toUpperCase();
-  car = car.toLowerCase();
-  car = car.normalize('NFC');
-   document.transcription.text2.value=translit;
-   document.transcription.text1.value=car;
-}
-function cyrlat () {
-car = document.transcription.text1.value;
-translit = document.transcription.text1.value;
-  translit = translit.toLowerCase();
-  car = car.toUpperCase();
-  car = car.normalize('NFC');
-document.transcription.text1.value=translit;
-document.transcription.text2.value=car;
-}
-function copy1()
-{ navigator.clipboard.writeText(document.transcription.text1.value);
+function latcyr() {
+	document.transcription.text2.value = document.transcription.text2.value
+		.toUpperCase();
+
+	document.transcription.text1.value = document.transcription.text2.value
+		.toLowerCase()
+		.normalize('NFC');
 }
 
-function copy2()
-{ navigator.clipboard.writeText(document.transcription.text2.value);
+function cyrlat() {
+	document.transcription.text1.value = document.transcription.text1.value
+		.toLowerCase();
+
+	document.transcription.text2.value = document.transcription.text1.value
+		.toUpperCase()
+		.normalize('NFC');
+}
+
+function copy1() {
+	navigator.clipboard.writeText(document.transcription.text1.value);
+}
+
+function copy2() {
+	navigator.clipboard.writeText(document.transcription.text2.value);
 }
