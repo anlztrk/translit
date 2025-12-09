8e8e7089d1f802bb9fea2f4152530d383c09fdbf
anlztrk <64364162+anlztrk@users.noreply.github.com>
2025-12-09 11:46:02 +0300

    Update conversion.js

--- az/alt/conversion.js
+++ az/alt/conversion.js
@@ -1,25 +1,23 @@
-﻿
-var car;
-function cyrlat () {
-car = document.transcription.text1.value;
-  car = car.replace(/e/g, "ë");
-  car = car.replace(/E/g, "Ë");
-  car = car.replace(/ə/g, "e");
-  car = car.replace(/Ə/g, "E");
-document.transcription.text2.value=car;
-}
-function latcyr () {
-car = document.transcription.text2.value;
-  car = car.replace(/e/g, "ə");
-  car = car.replace(/E/g, "Ə");
-  car = car.replace(/ë/g, "e");
-  car = car.replace(/Ë/g, "E");
-document.transcription.text1.value=car;
-}
-function copy1()
-{ navigator.clipboard.writeText(document.transcription.text1.value);
+function cyrlat() {
+	document.transcription.text2.value = document.transcription.text1.value
+		.replace(/e/g, "ë")
+		.replace(/E/g, "Ë")
+		.replace(/ə/g, "e")
+		.replace(/Ə/g, "E");
 }
 
-function copy2()
-{ navigator.clipboard.writeText(document.transcription.text2.value);
+function latcyr() {
+	document.transcription.text1.value = document.transcription.text2.value
+		.replace(/e/g, "ə")
+		.replace(/E/g, "Ə")
+		.replace(/ë/g, "e")
+		.replace(/Ë/g, "E");
+}
+
+function copy1() {
+	navigator.clipboard.writeText(document.transcription.text1.value);
+}
+
+function copy2() {
+	navigator.clipboard.writeText(document.transcription.text2.value);
 }
