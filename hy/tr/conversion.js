4fb7999356dddbec7b9aba825eaaf367bd66b4de
anlztrk <64364162+anlztrk@users.noreply.github.com>
2025-12-10 17:33:03 +0300

    Update conversion.js

--- hy/tr/conversion.js
+++ hy/tr/conversion.js
@@ -1,102 +1,101 @@
-var car;
-function armlat () {
-car = document.transcription.text1.value;
-car = car.replace(/Ե(Ւ|ւ)/g, "Վ");
-car = car.replace(/եւ/g, "վ");
-car = car.replace(/ԵՎ/g, "EW");
-car = car.replace(/Եվ/g, "Ew");
-car = car.replace(/Ու/g, "U");
-car = car.replace(/ՈՒ/g, "U");
-car = car.replace(/ու/g, "u");
-car = car.replace(/Ա/g, "A");
-car = car.replace(/Բ/g, "B"); //b/
-car = car.replace(/Գ/g, "G"); //g/
-car = car.replace(/Դ/g, "D"); //d/
-car = car.replace(/Ե/g, "E");
-car = car.replace(/Զ/g, "Z");
-car = car.replace(/Է/g, "Ê");
-car = car.replace(/Ը/g, "I");
-car = car.replace(/Թ/g, "T"); //tʰ/
-car = car.replace(/Ժ/g, "J");
-car = car.replace(/Ի/g, "Î");
-car = car.replace(/Լ/g, "L");
-car = car.replace(/Խ/g, "X");
-car = car.replace(/Ծ/g, "Ǯ"); //ts/
-car = car.replace(/Կ/g, "Ǩ"); //k/
-car = car.replace(/Հ/g, "H");
-car = car.replace(/Ձ/g, "Ž"); //dz/
-car = car.replace(/Ղ/g, "Q");
-car = car.replace(/Ճ/g, "Ç̌"); //tʃ/
-car = car.replace(/Մ/g, "M");
-car = car.replace(/Յ/g, "Y");
-car = car.replace(/Ն/g, "N");
-car = car.replace(/Շ/g, "Ş");
-car = car.replace(/Ո/g, "O");
-car = car.replace(/Չ/g, "Ç"); //tʃʰ/
-car = car.replace(/Պ/g, "P̌"); //p/
-car = car.replace(/Ջ/g, "C"); //dʒ/
-car = car.replace(/Ռ/g, "Ř");
-car = car.replace(/Ս/g, "S");
-car = car.replace(/Վ/g, "V");
-car = car.replace(/Տ/g, "Ť"); //t/
-car = car.replace(/Ր/g, "R");
-car = car.replace(/Ց/g, "Ʒ"); //tsʰ/
-car = car.replace(/Ւ/g, "W");
-car = car.replace(/Փ/g, "P"); //pʰ/
-car = car.replace(/Ք/g, "K"); //kʰ/
-car = car.replace(/Օ/g, "Û");
-car = car.replace(/Ֆ/g, "F");
-car = car.replace(/ա/g, "a");
-car = car.replace(/բ/g, "b"); //b/
-car = car.replace(/գ/g, "g"); //g/
-car = car.replace(/դ/g, "d"); //d/
-car = car.replace(/ե/g, "e");
-car = car.replace(/զ/g, "z");
-car = car.replace(/է/g, "ê");
-car = car.replace(/ը/g, "i");
-car = car.replace(/թ/g, "t"); //tʰ/
-car = car.replace(/ժ/g, "j");
-car = car.replace(/ի/g, "î");
-car = car.replace(/լ/g, "l");
-car = car.replace(/խ/g, "x");
-car = car.replace(/ծ/g, "ǯ"); //ts/
-car = car.replace(/կ/g, "ǩ"); //k/
-car = car.replace(/հ/g, "h");
-car = car.replace(/ձ/g, "ž"); //dz/
-car = car.replace(/ղ/g, "q");
-car = car.replace(/ճ/g, "ç̌"); //tʃ/
-car = car.replace(/մ/g, "m");
-car = car.replace(/յ/g, "y");
-car = car.replace(/ն/g, "n");
-car = car.replace(/շ/g, "ş");
-car = car.replace(/ո/g, "o");
-car = car.replace(/չ/g, "ç"); //tʃʰ/
-car = car.replace(/պ/g, "p̌"); //p/
-car = car.replace(/ջ/g, "c"); //dʒ/
-car = car.replace(/ռ/g, "ř");
-car = car.replace(/ս/g, "s");
-car = car.replace(/վ/g, "v");
-car = car.replace(/տ/g, "t‌̌"); //t/
-car = car.replace(/ր/g, "r");
-car = car.replace(/ց/g, "ʒ"); //tsʰ/
-car = car.replace(/ւ/g, "w");
-car = car.replace(/փ/g, "p"); //pʰ/
-car = car.replace(/ք/g, "k"); //kʰ/
-car = car.replace(/և/g, "ew");
-car = car.replace(/օ/g, "û");
-car = car.replace(/ֆ/g, "f");
-car = car.replace(/(\u055b|\u055c|\u055e)/g, "");
-car = car.replace(/(\u055d|\u0060)/g, "\u003b");
-car = car.replace(/(\u003a|\u0589)/g, "\u002e");
-car = car.replace(/\u2024/g, "\u003a");
-car = car.replace(/\u058a/g, "\u002d");
-car = car.replace(/\u055a/g, "\u2019");
-document.transcription.text2.value=car;
-}
-function copy1()
-{ navigator.clipboard.writeText(document.transcription.text1.value);
+function armlat() {
+	document.transcription.text2.value = document.transcription.text1.value
+		.replace(/Ե(Ւ|ւ)/g, "Վ")
+		.replace(/եւ/g, "վ")
+		.replace(/ԵՎ/g, "EW")
+		.replace(/Եվ/g, "Ew")
+		.replace(/Ու/g, "U")
+		.replace(/ՈՒ/g, "U")
+		.replace(/ու/g, "u")
+		.replace(/Ա/g, "A")
+		.replace(/Բ/g, "B") //b/
+		.replace(/Գ/g, "G") //g/
+		.replace(/Դ/g, "D") //d/
+		.replace(/Ե/g, "E")
+		.replace(/Զ/g, "Z")
+		.replace(/Է/g, "Ê")
+		.replace(/Ը/g, "I")
+		.replace(/Թ/g, "T") //tʰ/
+		.replace(/Ժ/g, "J")
+		.replace(/Ի/g, "Î")
+		.replace(/Լ/g, "L")
+		.replace(/Խ/g, "X")
+		.replace(/Ծ/g, "Ǯ") //ts/
+		.replace(/Կ/g, "Ǩ") //k/
+		.replace(/Հ/g, "H")
+		.replace(/Ձ/g, "Ž") //dz/
+		.replace(/Ղ/g, "Q")
+		.replace(/Ճ/g, "Ç̌") //tʃ/
+		.replace(/Մ/g, "M")
+		.replace(/Յ/g, "Y")
+		.replace(/Ն/g, "N")
+		.replace(/Շ/g, "Ş")
+		.replace(/Ո/g, "O")
+		.replace(/Չ/g, "Ç") //tʃʰ/
+		.replace(/Պ/g, "P̌") //p/
+		.replace(/Ջ/g, "C") //dʒ/
+		.replace(/Ռ/g, "Ř")
+		.replace(/Ս/g, "S")
+		.replace(/Վ/g, "V")
+		.replace(/Տ/g, "Ť") //t/
+		.replace(/Ր/g, "R")
+		.replace(/Ց/g, "Ʒ") //tsʰ/
+		.replace(/Ւ/g, "W")
+		.replace(/Փ/g, "P") //pʰ/
+		.replace(/Ք/g, "K") //kʰ/
+		.replace(/Օ/g, "Û")
+		.replace(/Ֆ/g, "F")
+		.replace(/ա/g, "a")
+		.replace(/բ/g, "b") //b/
+		.replace(/գ/g, "g") //g/
+		.replace(/դ/g, "d") //d/
+		.replace(/ե/g, "e")
+		.replace(/զ/g, "z")
+		.replace(/է/g, "ê")
+		.replace(/ը/g, "i")
+		.replace(/թ/g, "t") //tʰ/
+		.replace(/ժ/g, "j")
+		.replace(/ի/g, "î")
+		.replace(/լ/g, "l")
+		.replace(/խ/g, "x")
+		.replace(/ծ/g, "ǯ") //ts/
+		.replace(/կ/g, "ǩ") //k/
+		.replace(/հ/g, "h")
+		.replace(/ձ/g, "ž") //dz/
+		.replace(/ղ/g, "q")
+		.replace(/ճ/g, "ç̌") //tʃ/
+		.replace(/մ/g, "m")
+		.replace(/յ/g, "y")
+		.replace(/ն/g, "n")
+		.replace(/շ/g, "ş")
+		.replace(/ո/g, "o")
+		.replace(/չ/g, "ç") //tʃʰ/
+		.replace(/պ/g, "p̌") //p/
+		.replace(/ջ/g, "c") //dʒ/
+		.replace(/ռ/g, "ř")
+		.replace(/ս/g, "s")
+		.replace(/վ/g, "v")
+		.replace(/տ/g, "t‌̌") //t/
+		.replace(/ր/g, "r")
+		.replace(/ց/g, "ʒ") //tsʰ/
+		.replace(/ւ/g, "w")
+		.replace(/փ/g, "p") //pʰ/
+		.replace(/ք/g, "k") //kʰ/
+		.replace(/և/g, "ew")
+		.replace(/օ/g, "û")
+		.replace(/ֆ/g, "f")
+		.replace(/(\u055b|\u055c|\u055e)/g, "")
+		.replace(/(\u055d|\u0060)/g, "\u003b")
+		.replace(/(\u003a|\u0589)/g, "\u002e")
+		.replace(/\u2024/g, "\u003a")
+		.replace(/\u058a/g, "\u002d")
+		.replace(/\u055a/g, "\u2019");
 }
 
-function copy2()
-{ navigator.clipboard.writeText(document.transcription.text2.value);
+function copy1() {
+	navigator.clipboard.writeText(document.transcription.text1.value);
+}
+
+function copy2() {
+	navigator.clipboard.writeText(document.transcription.text2.value);
 }
