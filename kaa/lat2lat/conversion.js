7eced9ef089f46a744e8d3bf136334da338cfde9
anlztrk <64364162+anlztrk@users.noreply.github.com>
2025-12-12 12:14:37 +0300

    Refactor transcription conversion functions

--- kaa/lat2lat/conversion.js
+++ kaa/lat2lat/conversion.js
@@ -1,57 +1,54 @@
-var car;
 function latcyr () {
-car = document.transcription.text2.value;  
-  car = car.replace(/Ch/g, "C\u2019h");
-  car = car.replace(/CH/g, "C\u2019H");
-  car = car.replace(/ch/g, "c\u2019h");
-  car = car.replace(/Sh/g, "S\u2019h");
-  car = car.replace(/SH/g, "S\u2019H");
-  car = car.replace(/sh/g, "s\u2019h");
-  car = car.replace(/ç/g, "ch");
-  car = car.replace(/Ç/g, "Ch");
-  car = car.replace(/ş/g, "sh");
-  car = car.replace(/Ş/g, "Sh");
-  car = car.replace(/ğ/g, "ǵ");
-  car = car.replace(/Ğ/g, "Ǵ");
-  car = car.replace(/ñ/g, "ń");
-  car = car.replace(/Ñ/g, "Ń");
-  car = car.replace(/I/g, "Í");
-  car = car.replace(/İ/g, "I");
-  car = car.replace(/ä/g, "á");
-  car = car.replace(/Ä/g, "Á");
-  car = car.replace(/ö/g, "ó");
-  car = car.replace(/Ö/g, "Ó");
-  car = car.replace(/ü/g, "ú");
-  car = car.replace(/Ü/g, "Ú");
-document.transcription.text1.value=car;
+document.transcription.text1.value = document.transcription.text2.value  
+  .replace(/Ch/g, "C\u2019h")
+  .replace(/CH/g, "C\u2019H")
+  .replace(/ch/g, "c\u2019h")
+  .replace(/Sh/g, "S\u2019h")
+  .replace(/SH/g, "S\u2019H")
+  .replace(/sh/g, "s\u2019h")
+  .replace(/ç/g, "ch")
+  .replace(/Ç/g, "Ch")
+  .replace(/ş/g, "sh")
+  .replace(/Ş/g, "Sh")
+  .replace(/ğ/g, "ǵ")
+  .replace(/Ğ/g, "Ǵ")
+  .replace(/ñ/g, "ń")
+  .replace(/Ñ/g, "Ń")
+  .replace(/I/g, "Í")
+  .replace(/İ/g, "I")
+  .replace(/ä/g, "á")
+  .replace(/Ä/g, "Á")
+  .replace(/ö/g, "ó")
+  .replace(/Ö/g, "Ó")
+  .replace(/ü/g, "ú")
+  .replace(/Ü/g, "Ú");
 }
 function cyrlat () {
-car = document.transcription.text1.value;
-  car = car.replace(/Ch/g, "Ç");
-  car = car.replace(/CH/g, "Ç");
-  car = car.replace(/ch/g, "ç");
-  car = car.replace(/Sh/g, "Ş");
-  car = car.replace(/SH/g, "Ş");
-  car = car.replace(/sh/g, "ş");
-  car = car.replace(/C\u2019h/g, "Ch");
-  car = car.replace(/C\u2019H/g, "CH");
-  car = car.replace(/c\u2019h/g, "ch");
-  car = car.replace(/S\u2019h/g, "Sh");
-  car = car.replace(/S\u2019H/g, "SH");
-  car = car.replace(/s\u2019h/g, "sh");
-  car = car.replace(/ǵ/g, "ğ");
-  car = car.replace(/Ǵ/g, "Ğ");
-  car = car.replace(/ń/g, "ñ");
-  car = car.replace(/Ń/g, "Ñ");
-  car = car.replace(/I/g, "İ");
-  car = car.replace(/Í/g, "I");
-  car = car.replace(/á/g, "ä");
-  car = car.replace(/Á/g, "Ä");
-  car = car.replace(/ó/g, "ö");
-  car = car.replace(/Ó/g, "Ö");
-  car = car.replace(/ú/g, "ü");
-  car = car.replace(/Ú/g, "Ü");  
-document.transcription.text2.value=car;
+document.transcription.text2.value = document.transcription.text1.value
+  .replace(/Ch/g, "Ç")
+  .replace(/CH/g, "Ç")
+  .replace(/ch/g, "ç")
+  .replace(/Sh/g, "Ş")
+  .replace(/SH/g, "Ş")
+  .replace(/sh/g, "ş")
+  .replace(/C\u2019h/g, "Ch")
+  .replace(/C\u2019H/g, "CH")
+  .replace(/c\u2019h/g, "ch")
+  .replace(/S\u2019h/g, "Sh")
+  .replace(/S\u2019H/g, "SH")
+  .replace(/s\u2019h/g, "sh")
+  .replace(/ǵ/g, "ğ")
+  .replace(/Ǵ/g, "Ğ")
+  .replace(/ń/g, "ñ")
+  .replace(/Ń/g, "Ñ")
+  .replace(/I/g, "İ")
+  .replace(/Í/g, "I")
+  .replace(/á/g, "ä")
+  .replace(/Á/g, "Ä")
+  .replace(/ó/g, "ö")
+  .replace(/Ó/g, "Ö")
+  .replace(/ú/g, "ü")
+  .replace(/Ú/g, "Ü");
 }
 function copy1()
 { navigator.clipboard.writeText(document.transcription.text1.value);
