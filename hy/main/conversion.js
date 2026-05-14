87127a1cab778db04c2a6b4bf92ec0c6d85e216f
anlztrk <64364162+anlztrk@users.noreply.github.com>
2026-05-14 12:16:55 +0300

    Rename armlat function to topright and add topleft

--- hy/main/conversion.js
+++ hy/main/conversion.js
@@ -1,12 +1,8 @@
-function armlat() {
+function topright() {
 	document.transcription.text2.value = document.transcription.text1.value
 		.replace(/Ե([Ււ])/g, "Վ")
 		.replace(/եւ/g, "վ")
-
-		.replace(/([ԲԳԴԶԹԺԽԾԿՀՁՂՃՄՅՆՇՉՊՋՌՍՎՏՐՑՓՔՖ])([ԲԳԴԶԹԺԽԾԿՀՁՂՃՄՅՆՇՉՊՋՌՍՎՏՐՑՓՔՖ])([ԲԳԴԶԹԺԽԾԿՀՁՂՃՄՅՆՇՉՊՋՌՍՎՏՐՑՓՔՖ])/g, "$1Ը$2$3")
-		.replace(/([բգդզթժխծկհձղճմյնշչպջռսվտրցփքֆ])([բգդզթժխծկհձղճմյնշչպջռսվտրցփքֆ])([բգդզթժխծկհձղճմյնշչպջռսվտրցփքֆ])/g, "$1ը$2$3")
-
-
+		
 		.replace(/ԵՎ/g, "EW")
 		.replace(/Եվ/g, "Ew")
 		.replace(/Ու/g, "U")
@@ -88,6 +84,106 @@ function armlat() {
 		.replace(/ք/g, "k") //kʰ/
 		.replace(/և/g, "ew")
 		.replace(/օ/g, "ó")
+		.replace(/ֆ/g, "f")
+		.replace(/(\u055b|\u055c|\u055e)/g, "")
+		.replace(/(\u055d|\u0060)/g, "\u003b")
+		.replace(/(\u003a|\u0589)/g, "\u002e")
+		.replace(/\u2024/g, "\u003a")
+		.replace(/\u058a/g, "\u002d")
+		.replace(/\u055a/g, "\u2019");
+}
+
+function topleft() {
+	document.transcription.text2.value = document.transcription.text1.value
+		.replace(/Ե([Ււ])/g, "Վ")
+		.replace(/եւ/g, "վ")
+
+		.replace(/(\p{Script=Armn})Ե/ug, "$1Է")
+		.replace(/(\p{Script=Armn})ե/ug, "$1է")
+		.replace(/(\p{Script=Armn})Ո/ug, "$1Օ")
+		.replace(/(\p{Script=Armn})ո/ug, "$1օ")
+		
+		.replace(/ԵՎ/g, "EV")
+		.replace(/Եվ/g, "Ev")
+		.replace(/Ու/g, "U")
+		.replace(/ՈՒ/g, "U")
+		.replace(/ու/g, "u")
+		.replace(/Ա/g, "A")
+		.replace(/Բ/g, "B") //b/
+		.replace(/Գ/g, "G") //g/
+		.replace(/Դ/g, "D") //d/
+		.replace(/Ե/g, "Ye")
+		.replace(/Զ/g, "Z")
+		.replace(/Է/g, "E")
+		.replace(/Ը/g, "Y")
+		.replace(/Թ/g, "Th") //tʰ/
+		.replace(/Ժ/g, "Zh")
+		.replace(/Ի/g, "I")
+		.replace(/Լ/g, "L")
+		.replace(/Խ/g, "Kh")
+		.replace(/Ծ/g, "Ts") //ts/
+		.replace(/Կ/g, "K") //k/
+		.replace(/Հ/g, "H")
+		.replace(/Ձ/g, "Dz") //dz/
+		.replace(/Ղ/g, "Gh")
+		.replace(/Ճ/g, "Tch") //tʃ/
+		.replace(/Մ/g, "M")
+		.replace(/Յ/g, "Y")
+		.replace(/Ն/g, "N")
+		.replace(/Շ/g, "Sh")
+		.replace(/Ո/g, "Vo")
+		.replace(/Չ/g, "Ch") //tʃʰ/
+		.replace(/Պ/g, "P") //p/
+		.replace(/Ջ/g, "J") //dʒ/
+		.replace(/Ռ/g, "R")
+		.replace(/Ս/g, "S")
+		.replace(/Վ/g, "V")
+		.replace(/Տ/g, "T") //t/
+		.replace(/Ր/g, "R")
+		.replace(/Ց/g, "Ts") //tsʰ/
+		.replace(/Ւ/g, "V")
+		.replace(/Փ/g, "Ph") //pʰ/
+		.replace(/Ք/g, "Q") //kʰ/
+		.replace(/Օ/g, "O")
+		.replace(/Ֆ/g, "F")
+		.replace(/ա/g, "a")
+		.replace(/բ/g, "b") //b/
+		.replace(/գ/g, "g") //g/
+		.replace(/դ/g, "d") //d/
+		.replace(/ե/g, "ye")
+		.replace(/զ/g, "z")
+		.replace(/է/g, "e")
+		.replace(/ը/g, "y")
+		.replace(/թ/g, "th") //tʰ/
+		.replace(/ժ/g, "j")
+		.replace(/ի/g, "i")
+		.replace(/լ/g, "l")
+		.replace(/խ/g, "kh")
+		.replace(/ծ/g, "ts") //ts/
+		.replace(/կ/g, "k") //k/
+		.replace(/հ/g, "h")
+		.replace(/ձ/g, "dz") //dz/
+		.replace(/ղ/g, "gh")
+		.replace(/ճ/g, "tch") //tʃ/
+		.replace(/մ/g, "m")
+		.replace(/յ/g, "y")
+		.replace(/ն/g, "n")
+		.replace(/շ/g, "sh")
+		.replace(/ո/g, "vo")
+		.replace(/չ/g, "ch") //tʃʰ/
+		.replace(/պ/g, "p") //p/
+		.replace(/ջ/g, "j") //dʒ/
+		.replace(/ռ/g, "r")
+		.replace(/ս/g, "s")
+		.replace(/վ/g, "v")
+		.replace(/տ/g, "t") //t/
+		.replace(/ր/g, "r")
+		.replace(/ց/g, "ts") //tsʰ/
+		.replace(/ւ/g, "v")
+		.replace(/փ/g, "ph") //pʰ/
+		.replace(/ք/g, "q") //kʰ/
+		.replace(/և/g, "ev")
+		.replace(/օ/g, "o")
 		.replace(/ֆ/g, "f")
 		.replace(/(\u055b|\u055c|\u055e)/g, "")
 		.replace(/(\u055d|\u0060)/g, "\u003b")
