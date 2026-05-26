6f2fecf5a8c3cb42ea30f08a611da51b5e979a50
anlztrk <64364162+anlztrk@users.noreply.github.com>
2026-05-26 10:25:38 +0300

    Refactor transcription functions for clarity and efficiency

--- lx/translator/conversion.js
+++ lx/translator/conversion.js
@@ -1,695 +1,198 @@
-﻿function latcyr() {
-
-	document.transcription.text1.value = document.transcription.text2.value
-
-		.replace(/^je/g, "е")
-
-		.replace(/^je/g, "е")
-
-		.replace(/\nje/g, "\nе")
-
-		.replace(/ je/g, " е")
-
-		.replace(/^Je/g, "Е")
-
-		.replace(/\nJe/g, "\nЕ")
-
-		.replace(/ Je/g, " Е")
-
-		.replace(/^JE/g, "Е")
-
-		.replace(/\nJE/g, "\nЕ")
-
-		.replace(/ JE/g, " Е")
-
-		.replace(/aje/g, "ае")
-
-		.replace(/áje/g, "яе")
-
-		.replace(/eje/g, "эе")
-
-		.replace(/ije/g, "ие")
-
-		.replace(/yje/g, "іе")
-
-		.replace(/oje/g, "ое")
-
-		.replace(/óje/g, "ёе")
-
-		.replace(/uje/g, "уе")
-
-		.replace(/úje/g, "юе")
-
-		.replace(/Aje/g, "Ае")
-
-		.replace(/Áje/g, "Яе")
-
-		.replace(/Eje/g, "Эе")
-
-		.replace(/Ije/g, "Ие")
-
-		.replace(/Yje/g, "Іе")
-
-		.replace(/Oje/g, "Ое")
-
-		.replace(/Óje/g, "Ёе")
-
-		.replace(/Uje/g, "Уе")
-
-		.replace(/Úje/g, "Юе")
-
-		.replace(/AJE/g, "АЕ")
-
-		.replace(/ÁJE/g, "ЯЕ")
-
-		.replace(/EJE/g, "ЭЕ")
-
-		.replace(/IJE/g, "ИЕ")
-
-		.replace(/YJE/g, "ІЕ")
-
-		.replace(/OJE/g, "ОЕ")
-
-		.replace(/ÓJE/g, "ЁЕ")
-
-		.replace(/UJE/g, "УЕ")
-
-		.replace(/ÚJE/g, "ЮЕ")
-
-		.replace(/'/g, "-")
-
-		.replace(/“/g, "«")
-
-		.replace(/”/g, "»")
-
-		.replace(/c-h/g, "цг")
-
-		.replace(/C-h/g, "Цг")
-
-		.replace(/C-H/g, "ЦГ")
-
-		.replace(/k-h/g, "қг")
-
-		.replace(/K-h/g, "Қг")
-
-		.replace(/K-H/g, "ҚГ")
-
-		.replace(/s-h/g, "сг")
-
-		.replace(/S-h/g, "Сг")
-
-		.replace(/S-H/g, "СГ")
-
-		.replace(/z-h/g, "зг")
-
-		.replace(/Z-h/g, "Зг")
-
-		.replace(/Z-H/g, "ЗГ")
-
-		.replace(/u/g, "у")
-
-		.replace(/kh/g, "х")
-
-		.replace(/j/g, "й")
-
-		.replace(/m/g, "м")
-
-		.replace(/i/g, "и")
-
-		.replace(/o/g, "о")
-
-		.replace(/ó/g, "ё")
-
-		.replace(/g/g, "ґ")
-
-		.replace(/n/g, "н")
-
-		.replace(/sh/g, "ш")
-
-		.replace(/ch/g, "щ")
-
-		.replace(/zh/g, "ж")
-
-		.replace(/dж/g, "ч")
-
-		.replace(/s/g, "с")
-
-		.replace(/y/g, "і")
-
-		.replace(/b/g, "б")
-
-		.replace(/f/g, "ф")
-
-		.replace(/v/g, "в")
-
-		.replace(/w/g, "ў")
-
-		.replace(/q/g, "к")
-
-		.replace(/t/g, "т")
-
-		.replace(/l/g, "л")
-
-		.replace(/e/g, "э")
-
-		.replace(/ú/g, "ю")
-
-		.replace(/c/g, "ц")
-
-		.replace(/x/g, "ѕ")
-
-		.replace(/z/g, "з")
-
-		.replace(/a/g, "а")
-
-		.replace(/k/g, "қ")
-
-		.replace(/á/g, "я")
-
-		.replace(/p/g, "п")
-
-		.replace(/d/g, "д")
-
-		.replace(/r/g, "р")
-
-		.replace(/U/g, "У")
-
-		.replace(/é/g, "є")
-
-		.replace(/É/g, "Є")
-
-		.replace(/Kh/g, "Х")
-
-		.replace(/KH/g, "Х")
-
-		.replace(/J/g, "Й")
-
-		.replace(/M/g, "М")
-
-		.replace(/I/g, "И")
-
-		.replace(/O/g, "О")
-
-		.replace(/Ó/g, "Ё")
-
-		.replace(/G/g, "Ґ")
-
-		.replace(/N/g, "Н")
-
-		.replace(/Sh/g, "Ш")
-
-		.replace(/SH/g, "Ш")
-
-		.replace(/Ch/g, "Щ")
-
-		.replace(/CH/g, "Щ")
-
-		.replace(/Zh/g, "Ж")
-
-		.replace(/ZH/g, "Ж")
-
-		.replace(/Dж/g, "Ч")
-
-		.replace(/DЖ/g, "Ч")
-
-		.replace(/S/g, "С")
-
-		.replace(/Y/g, "І")
-
-		.replace(/B/g, "Б")
-
-		.replace(/F/g, "Ф")
-
-		.replace(/V/g, "В")
-
-		.replace(/W/g, "Ў")
-
-		.replace(/Q/g, "К")
-
-		.replace(/T/g, "Т")
-
-		.replace(/L/g, "Л")
-
-		.replace(/E/g, "Э")
-
-		.replace(/Ú/g, "Ю")
-
-		.replace(/C/g, "Ц")
-
-		.replace(/X/g, "Ѕ")
-
-		.replace(/Z/g, "З")
-
-		.replace(/A/g, "А")
-
-		.replace(/K/g, "Қ")
-
-		.replace(/Á/g, "Я")
-
-		.replace(/P/g, "П")
-
-		.replace(/D/g, "Д")
-
-		.replace(/R/g, "Р")
-
-		.replace(/h/g, "г")
-
-		.replace(/H/g, "Г")
-
-		.replace(/є/g, "эа")
-
-		.replace(/Є/g, "Эа")
-
-		.replace(/-/g, "'")
-
-		.replace(/«/g, "“")
-
-		.replace(/»/g, "”")
-
-		.replace(/Е/g, "Йэ")
-
-		.replace(/е/g, "йэ")
-
-		.replace(/Ч/g, "Дж")
-
-		.replace(/ч/g, "дж")
-
-		.replace(/Ѕ/g, "Дз")
-
-		.replace(/ѕ/g, "дз")
-
-		.replace(/Қ/g, "Хг")
-
-		.replace(/қ/g, "хг")
-
-		.replace(/Ў/g, "Лл")
-
-		.replace(/ў/g, "лл")
-
-		.replace(/Ж/g, "Зг")
-
-		.replace(/ж/g, "зг")
-
-		.replace(/Щ/g, "Тш")
-
-		.replace(/щ/g, "тш")
-
-		.replace(/у/g, "a")
-
-		.replace(/я/g, "à")
-
-		.replace(/ё/g, " '")
-
-		.replace(/х/g, "b")
-
-		.replace(/й/g, "c")
-
-		.replace(/м/g, "d")
-
-		.replace(/и/g, "e")
-
-		.replace(/о/g, "è")
-
-		.replace(/ґ/g, "f")
-
-		.replace(/н/g, "g")
-
-		.replace(/с/g, "h")
-
-		.replace(/і/g, "i")
-
-		.replace(/б/g, "j")
-
-		.replace(/п/g, "k")
-
-		.replace(/в/g, "l")
-
-		.replace(/т/g, "m")
-
-		.replace(/л/g, "n")
-
-		.replace(/э/g, "o")
-
-		.replace(/ю/g, "ò")
-
-		.replace(/ц/g, "p")
-
-		.replace(/г/g, "r")
-
-		.replace(/ш/g, "s")
-
-		.replace(/з/g, "t")
-
-		.replace(/а/g, "u")
-
-		.replace(/к/g, "v")
-
-		.replace(/ф/g, "w")
-
-		.replace(/д/g, "y")
-
-		.replace(/р/g, "z")
-
-		.replace(/У/g, "A")
-
-		.replace(/Я/g, "À")
-
-		.replace(/Ё/g, " '")
-
-		.replace(/Х/g, "B")
-
-		.replace(/Й/g, "C")
-
-		.replace(/М/g, "D")
-
-		.replace(/И/g, "E")
-
-		.replace(/О/g, "È")
-
-		.replace(/Ґ/g, "F")
-
-		.replace(/Н/g, "G")
-
-		.replace(/С/g, "H")
-
-		.replace(/І/g, "I")
-
-		.replace(/Б/g, "J")
-
-		.replace(/П/g, "K")
-
-		.replace(/В/g, "L")
-
-		.replace(/Т/g, "M")
-
-		.replace(/Л/g, "N")
-
-		.replace(/Э/g, "O")
-
-		.replace(/Ю/g, "Ò")
-
-		.replace(/Ц/g, "P")
-
-		.replace(/Г/g, "R")
-
-		.replace(/Ш/g, "S")
-
-		.replace(/З/g, "T")
-
-		.replace(/А/g, "U")
-
-		.replace(/К/g, "V")
-
-		.replace(/Ф/g, "W")
-
-		.replace(/Д/g, "Y")
-
-		.replace(/Р/g, "Z");
-
-}
-
 function cyrlat() {
-
 	document.transcription.text2.value = document.transcription.text1.value
-
-		.replace(/ '/g, "ё")
-
-		.replace(/'/g, "-")
-
-		.replace(/<</g, "«")
-
-		.replace(/>>/g, "»")
-
-		.replace(/“/g, "«")
-
-		.replace(/”/g, "»")
-
-		.replace(/a/g, "у")
-
-		.replace(/à/g, "я")
-
-		.replace(/ä/g, "я")
-
-		.replace(/b/g, "х")
-
-		.replace(/c/g, "й")
-
-		.replace(/d/g, "м")
-
-		.replace(/e/g, "и")
-
-		.replace(/è/g, "о")
-
-		.replace(/é/g, "о")
-
-		.replace(/ê/g, "о")
-
-		.replace(/f/g, "ґ")
-
-		.replace(/g/g, "н")
-
-		.replace(/h/g, "с")
-
-		.replace(/i/g, "і")
-
-		.replace(/j/g, "б")
-
-		.replace(/k/g, "п")
-
-		.replace(/l/g, "в")
-
-		.replace(/m/g, "т")
-
-		.replace(/n/g, "л")
-
-		.replace(/o/g, "э")
-
-		.replace(/ò/g, "ю")
-
-		.replace(/ô/g, "ю")
-
-		.replace(/p/g, "ц")
-
-		.replace(/r/g, "г")
-
-		.replace(/s/g, "ш")
-
-		.replace(/t/g, "з")
-
-		.replace(/u/g, "а")
-
-		.replace(/v/g, "к")
-
-		.replace(/w/g, "ф")
-
-		.replace(/x/g, "пш")
-
-		.replace(/q/g, "п")
-
-		.replace(/y/g, "д")
-
-		.replace(/z/g, "р")
-
-		.replace(/A/g, "У")
-
-		.replace(/À/g, "Я")
-
-		.replace(/Ä/g, "Я")
-
-		.replace(/B/g, "Х")
-
-		.replace(/C/g, "Й")
-
-		.replace(/D/g, "М")
-
-		.replace(/E/g, "И")
-
-		.replace(/È/g, "О")
-
-		.replace(/É/g, "О")
-
-		.replace(/Ê/g, "О")
-
-		.replace(/F/g, "Ґ")
-
-		.replace(/G/g, "Н")
-
-		.replace(/H/g, "С")
-
-		.replace(/I/g, "І")
-
-		.replace(/J/g, "Б")
-
-		.replace(/K/g, "П")
-
-		.replace(/L/g, "В")
-
-		.replace(/M/g, "Т")
-
-		.replace(/N/g, "Л")
-
-		.replace(/O/g, "Э")
-
-		.replace(/Ò/g, "Ю")
-
-		.replace(/Ô/g, "Ю")
-
-		.replace(/P/g, "Ц")
-
-		.replace(/R/g, "Г")
-
-		.replace(/S/g, "Ш")
-
-		.replace(/T/g, "З")
-
-		.replace(/U/g, "А")
-
-		.replace(/V/g, "К")
-
-		.replace(/W/g, "Ф")
-
-		.replace(/X/g, "Пш")
-
-		.replace(/Q/g, "П")
-
-		.replace(/Y/g, "Д")
-
-		.replace(/Z/g, "Р")
-
-		.replace(/ХГ/g, "Қ")
-
-		.replace(/Хг/g, "Қ")
-
-		.replace(/хг/g, "қ")
-
-		.replace(/ЛЛ/g, "Ў")
-
-		.replace(/Лл/g, "Ў")
-
-		.replace(/лл/g, "ў")
-
-		.replace(/ЗГ/g, "Ж")
-
-		.replace(/Зг/g, "Ж")
-
-		.replace(/зг/g, "ж")
-
-		.replace(/ТШ/g, "Щ")
-
-		.replace(/Тш/g, "Щ")
-
-		.replace(/тш/g, "щ")
-
-		.replace(/ТС/g, "Ц")
-
-		.replace(/Тс/g, "Ц")
-
-		.replace(/тс/g, "ц")
-
-		.replace(/ДЖ/g, "Ч")
-
-		.replace(/Дж/g, "Ч")
-
-		.replace(/дж/g, "ч")
-
-		.replace(/ДЗ/g, "Ѕ")
-
-		.replace(/Дз/g, "Ѕ")
-
-		.replace(/дз/g, "ѕ")
-
-		.replace(/^йэ/g, "е")
-
-		.replace(/\nйэ/g, "\nе")
-
-		.replace(/ йэ/g, " е")
-
-		.replace(/^Йэ/g, "Е")
-
-		.replace(/\nЙэ/g, "\nЕ")
-
-		.replace(/ Йэ/g, " Е")
-
-		.replace(/^ЙЭ/g, "Е")
-
-		.replace(/\nЙЭ/g, "\nЕ")
-
-		.replace(/ ЙЭ/g, " Е")
-
-		.replace(/айэ/g, "ае")
-
-		.replace(/яйэ/g, "яе")
-
-		.replace(/эйэ/g, "эе")
-
-		.replace(/ийэ/g, "ие")
-
-		.replace(/ійэ/g, "іе")
-
-		.replace(/ойэ/g, "ое")
-
-		.replace(/ёйэ/g, "ёе")
-
-		.replace(/уйэ/g, "уе")
-
-		.replace(/юйэ/g, "юе")
-
-		.replace(/Айэ/g, "Ае")
-
-		.replace(/Яйэ/g, "Яе")
-
-		.replace(/Эйэ/g, "Эе")
-
-		.replace(/Ийэ/g, "Ие")
-
-		.replace(/Ійэ/g, "Іе")
-
-		.replace(/Ойэ/g, "Ое")
-
-		.replace(/Ёйэ/g, "Ёе")
-
-		.replace(/Уйэ/g, "Уе")
-
-		.replace(/Юйэ/g, "Юе")
-
-		.replace(/АЙЭ/g, "АЕ")
-
-		.replace(/ЯЙЭ/g, "ЯЕ")
-
-		.replace(/ЭЙЭ/g, "ЭЕ")
-
-		.replace(/ИЙЭ/g, "ИЕ")
-
-		.replace(/ІЙЭ/g, "ІЕ")
-
-		.replace(/ОЙЭ/g, "ОЕ")
-
-		.replace(/ЁЙЭ/g, "ЁЕ")
-
-		.replace(/УЙЭ/g, "УЕ")
-
-		.replace(/ЮЙЭ/g, "ЮЕ")
-
-		.replace(/эа/g, "є")
-
-		.replace(/Эа/g, "Є")
-
-		.replace(/ЭА/g, "Є");
-
+		.replace(/(\p{Lu})X/ug, '$1KZ')
+		.replace(/(\p{Lu})(\p{Lu}) X/ug, '$1$2 KZ')
+		.replace(/X (\p{Lu})(\p{Lu})/ug, 'KZ $1$2')
+		.replace(/X(\p{Lu})/ug, 'KZ$1')
+		.replace(/X/g, 'Kz')
+		.replace(/x/g, 'kz')
+		.replace(/(\p{Lu})Q/ug, '$1КЬ')
+		.replace(/(\p{Lu})(\p{Lu}) Q/ug, '$1$2 КЬ')
+		.replace(/Q (\p{Lu})(\p{Lu})/ug, 'КЬ $1$2')
+		.replace(/Q(\p{Lu})/ug, 'КЬ$1')
+		.replace(/Q/g, 'Кь')
+		.replace(/q/g, 'кь')
+		.replace(/(\p{Lu})Q/ug, '$1ХЬ')
+		.replace(/(\p{Lu})(\p{Lu}) Q/ug, '$1$2 ХЬ')
+		.replace(/Q (\p{Lu})(\p{Lu})/ug, 'ХЬ $1$2')
+		.replace(/Q(\p{Lu})/ug, 'ХЬ$1')
+		.replace(/Q/g, 'Хь')
+		.replace(/q/g, 'хь')
+
+		.replace(/O[Uu]/g, 'Є')
+		.replace(/ou/g, 'є')
+
+		.replace(/A/g, 'У')
+		.replace(/B/g, 'Х')
+		.replace(/C/g, 'Ј')
+		.replace(/D/g, 'М')
+		.replace(/E/g, 'И')
+		.replace(/É/g, 'Ё')
+		.replace(/È/g, 'О')
+		.replace(/Ê/g, 'Ӗ')
+		.replace(/F/g, 'Г')
+		.replace(/G/g, 'Н')
+		.replace(/H/g, 'С')
+		.replace(/I/g, 'І')
+		.replace(/J/g, 'Б')
+		.replace(/K/g, 'П')
+		.replace(/L/g, 'В')
+		.replace(/M/g, 'Т')
+		.replace(/N/g, 'Л')
+		.replace(/O/g, 'Э')
+		.replace(/Ò/g, 'Ю')
+		.replace(/Ô/g, 'Ю')
+		.replace(/P/g, 'Ц')
+		.replace(/S/g, 'Ш')
+		.replace(/T/g, 'З')
+		.replace(/U/g, 'А')
+		.replace(/V/g, 'К')
+		.replace(/W/g, 'Ф')
+		.replace(/Y/g, 'Д')
+		.replace(/Z/g, 'Р')
+		.replace(/a/g, 'у')
+		.replace(/b/g, 'х')
+		.replace(/c/g, 'ј')
+		.replace(/d/g, 'м')
+		.replace(/e/g, 'и')
+		.replace(/é/g, 'ё')
+		.replace(/è/g, 'о')
+		.replace(/ê/g, 'ё\u0300')
+		.replace(/f/g, 'г')
+		.replace(/g/g, 'н')
+		.replace(/h/g, 'с')
+		.replace(/i/g, 'і')
+		.replace(/j/g, 'б')
+		.replace(/k/g, 'п')
+		.replace(/l/g, 'в')
+		.replace(/m/g, 'т')
+		.replace(/n/g, 'л')
+		.replace(/o/g, 'э')
+		.replace(/ò/g, 'ю')
+		.replace(/ô/g, 'ю\u0300')
+		.replace(/p/g, 'ц')
+		.replace(/s/g, 'ш')
+		.replace(/t/g, 'з')
+		.replace(/u/g, 'а')
+		.replace(/v/g, 'к')
+		.replace(/w/g, 'ф')
+		.replace(/y/g, 'д')
+		.replace(/z/g, 'р')
+
+		.replace(/Ј[Аа]/g, 'Я')
+		.replace(/ја/g, 'я')
+		.replace(/Ј[Ээ]/g, 'Е')
+		.replace(/јэ/g, 'е')
+		.replace(/З(?:ХЬ|хь)/g, 'Ж')
+		.replace(/зхь/g, 'ж')
+		.replace(/Д[Жж]/g, 'Ч')
+		.replace(/дж/g, 'ч')
+		.replace(/Ш[Чч]/g, 'Щ')
+		.replace(/шч/g, 'щ')
+
+		.normalize('NFC');
 }
 
-function copy1()
+function latcyr() {
+	document.transcription.text1.value = document.transcription.text2.value
+		.replace(/(\p{Lu})Я/ug, '$1ЈА')
+		.replace(/(\p{Lu})(\p{Lu}) Я/ug, '$1$2 ЈА')
+		.replace(/Я (\p{Lu})(\p{Lu})/ug, 'ЈА $1$2')
+		.replace(/Я(\p{Lu})/ug, 'ЈА$1')
+		.replace(/Я/g, 'Ја')
+		.replace(/я/g, 'ја')
+		.replace(/(\p{Lu})Е/ug, '$1ЈЭ')
+		.replace(/(\p{Lu})(\p{Lu}) Е/ug, '$1$2 ЈЭ')
+		.replace(/Е (\p{Lu})(\p{Lu})/ug, 'ЈЭ $1$2')
+		.replace(/Е(\p{Lu})/ug, 'ЈЭ$1')
+		.replace(/Е/g, 'Јэ')
+		.replace(/е/g, 'јэ')
+		.replace(/(\p{Lu})Щ/ug, '$1ШЧ')
+		.replace(/(\p{Lu})(\p{Lu}) Щ/ug, '$1$2 ШЧ')
+		.replace(/Щ (\p{Lu})(\p{Lu})/ug, 'ШЧ $1$2')
+		.replace(/Щ(\p{Lu})/ug, 'ШЧ$1')
+		.replace(/Щ/g, 'Шч')
+		.replace(/щ/g, 'шч')
+		.replace(/(\p{Lu})Ч/ug, '$1ДЖ')
+		.replace(/(\p{Lu})(\p{Lu}) Ч/ug, '$1$2 ДЖ')
+		.replace(/Ч (\p{Lu})(\p{Lu})/ug, 'ДЖ $1$2')
+		.replace(/Ч(\p{Lu})/ug, 'ДЖ$1')
+		.replace(/Ч/g, 'Дж')
+		.replace(/ч/g, 'дж')
+		.replace(/(\p{Lu})Ж/ug, '$1ЗХЬ')
+		.replace(/(\p{Lu})(\p{Lu}) Ж/ug, '$1$2 ЗХЬ')
+		.replace(/Ж (\p{Lu})(\p{Lu})/ug, 'ЗХЬ $1$2')
+		.replace(/Ж(\p{Lu})/ug, 'ЗХЬ$1')
+		.replace(/Ж/g, 'Зхь')
+		.replace(/ж/g, 'зхь')
 
-{
+		.replace(/А/g, 'U')
+		.replace(/Б/g, 'J')
+		.replace(/В/g, 'L')
+		.replace(/Г/g, 'F')
+		.replace(/Д/g, 'Y')
+		.replace(/Ё\u0300/g, 'Ê')
+		.replace(/Ё/g, 'É')
+		.replace(/З/g, 'T')
+		.replace(/И/g, 'E')
+		.replace(/І/g, 'İ')
+		.replace(/Ј/g, 'C')
+		.replace(/К[Ьь]/g, 'Q')
+		.replace(/К/g, 'V')
+		.replace(/Л/g, 'N')
+		.replace(/М/g, 'D')
+		.replace(/Н/g, 'G')
+		.replace(/О/g, 'È')
+		.replace(/П/g, 'K')
+		.replace(/Р/g, 'Z')
+		.replace(/С/g, 'H')
+		.replace(/Т/g, 'M')
+		.replace(/У/g, 'A')
+		.replace(/Ф/g, 'W')
+		.replace(/Х[Ьь]/g, 'R')
+		.replace(/Х/g, 'B')
+		.replace(/Ц/g, 'P')
+		.replace(/Ш/g, 'S')
+		.replace(/Э/g, 'O')
+		.replace(/Ю\u0300/g, 'Ô')
+		.replace(/Ю/g, 'Ò')
+		.replace(/а/g, 'u')
+		.replace(/б/g, 'j')
+		.replace(/в/g, 'l')
+		.replace(/г/g, 'f')
+		.replace(/д/g, 'y')
+		.replace(/ё/g, 'é')
+		.replace(/ё/g, 'ê')
+		.replace(/з/g, 't')
+		.replace(/и/g, 'e')
+		.replace(/і/g, 'i')
+		.replace(/ј/g, 'c')
+		.replace(/к/g, 'v')
+		.replace(/кь/g, 'q')
+		.replace(/л/g, 'n')
+		.replace(/м/g, 'd')
+		.replace(/н/g, 'g')
+		.replace(/о/g, 'è')
+		.replace(/п/g, 'k')
+		.replace(/р/g, 'z')
+		.replace(/с/g, 'h')
+		.replace(/т/g, 'm')
+		.replace(/у/g, 'a')
+		.replace(/ф/g, 'w')
+		.replace(/х/g, 'b')
+		.replace(/хь/g, 'r')
+		.replace(/ц/g, 'p')
+		.replace(/ш/g, 's')
+		.replace(/э/g, 'o')
+		.replace(/ю/g, 'ò')
+		.replace(/ю/g, 'ô')
+		.normalize('NFC');
+}
+
+function copy1() {
 	navigator.clipboard.writeText(document.transcription.text1.value);
-
-
 }
 
-function copy2()
-
-{
+function copy2() {
 	navigator.clipboard.writeText(document.transcription.text2.value);
-
-
 }
