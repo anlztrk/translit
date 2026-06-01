073c11d99ad4707d36051b152877db003244aed1
anlztrk <64364162+anlztrk@users.noreply.github.com>
2026-06-01 17:37:11 +0300

    Fix syntax error in conversion.js

--- hi/conversion.js
+++ hi/conversion.js
@@ -210,7 +210,7 @@ function topleft() {
 		.replace(/\u096e/g, '\u0038')
 		.replace(/\u096f/g, '\u0039')
 		
-		.replace(/\u0303/g 'N')
+		.replace(/\u0303/g, 'N')
 		
 		.replace(/JÑ/g, 'GY')		
 		.replace(/X/g, 'KH')
