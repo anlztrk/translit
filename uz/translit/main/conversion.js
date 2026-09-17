efc87392977617c1c3b93dae9bd332dd2517bf86
anlztrk <64364162+anlztrk@users.noreply.github.com>
2026-09-17 17:38:12 +0300

    Fix regex for character transliteration
    Updated regex to include additional Unicode characters for accurate transliteration.

--- uz/translit/main/conversion.js
+++ uz/translit/main/conversion.js
@@ -161,7 +161,7 @@ function latcyr() {
 		.replace(/Y/g, "Й")
 		.replace(/Z/g, "З");
 
-		if (/[ÖĞŞÇöğşç]/.test(car_Orig) && !/[ОоГг]\u02bb/.test(car_Orig)) {
+		if (/[ÖĞŞÇöğşç]/.test(car_Orig) && !/[ОоГг][\u02bb\u2018]/.test(car_Orig)) {
 		document.transcription.text1.value = document.transcription.text1.value
 		.replace(/ç/g, "ч")
 		.replace(/ş/g, "ш")
